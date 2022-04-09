2ff69a38f00afbc782b218940e32c2c1d5c0118b
thdqudgns <thdqudgns0814@gmail.com>
2022-04-09 16:08:07 +0900

    18108번 1998년생인 내가 태국에서는 2541년생?!

---
+++ Baekjoon/src/algorithm/ch1/Main04.java
@@ -0,0 +1,12 @@
+package algorithm.ch1;
+
+
+// 18108번 1998년생인 내가 태국에서는 2541년생?!
+import java.util.Scanner;
+class Main04 {
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 int year = sc.nextInt();
+		 System.out.println( year - 543 );
+	}
+}
