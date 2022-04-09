219f8c9f90c69bdc7072de002aa63bf0c8ec1ce8
thdqudgns <thdqudgns0814@gmail.com>
2022-04-09 16:07:06 +0900

    10172번 개 풀이 - \와 "를 출력하기 위해서는 \를 앞에 붙여줘야 한다

---
+++ Baekjoon/src/algorithm/ch1/Main01.java
@@ -0,0 +1,17 @@
+package algorithm.ch1;
+
+//10172번 개
+class Main01 {
+
+	// \와 "를 출력하기 위해서는 \를 앞에 붙여줘야 한다
+	
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+
+	}
+
+}
