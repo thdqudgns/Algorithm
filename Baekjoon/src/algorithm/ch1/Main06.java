a753e48ca1529546258031e1766ac0fecae60f75
thdqudgns <thdqudgns0814@gmail.com>
2022-04-09 16:08:25 +0900

    2588번 곱셈

---
+++ Baekjoon/src/algorithm/ch1/Main06.java
@@ -0,0 +1,16 @@
+package algorithm.ch1;
+
+
+// 2588번 곱셈
+import java.util.Scanner;
+class Main06 {
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 int A = sc.nextInt();
+		 int B = sc.nextInt();
+		 System.out.println( A*(B%10) );		//A x 일의자리
+		 System.out.println( A*((B/10)%10) );	//A x 십의자리
+		 System.out.println( A*(B/100) );		//A x 백의자리
+		 System.out.println( A*B );
+	}
+}
