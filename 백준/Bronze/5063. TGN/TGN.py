32d4b199f9d89d16523a6276ea4bd2a65adaa5c2
송병훈 <thdqudgns9612@gmail.com>
2022-08-09 21:25:44 +0900

    [Bronze III] Title: TGN, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+for i in range(T):
+    r,e,c = map(int, input().split(' '))
+    if r < e-c:
+        print('advertise')
+    elif r == e-c:
+        print('does not matter')
+    elif r > e-c:
+        print('do not advertise')
