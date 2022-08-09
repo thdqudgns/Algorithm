958a759535ba383717406efa84fbc46343855237
송병훈 <thdqudgns9612@gmail.com>
2022-08-09 21:13:51 +0900

    [Bronze V] Title: 나머지, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -0,0 +1,11 @@
+# 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다
+input_data = input().split(' ')
+
+A = int(input_data[0])
+B = int(input_data[1])
+C = int(input_data[2])
+
+print((A+B)%C)
+print(((A%C) + (B%C))%C)
+print((A*B)%C)
+print(((A%C) * (B%C))%C)
