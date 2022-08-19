6c61afaf3035d715ed4fe128ed122182396bae24
송병훈 <thdqudgns9612@gmail.com>
2022-08-20 00:32:11 +0900

    [Bronze III] Title: 첫 글자를 대문자로, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.py
+++ 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.py
@@ -1,8 +1,5 @@
 n = int(input())
 for i in range(n):
-    w = list(map(str,input()))
-    if 97 <= ord(w[0]) <= 122:
-        w[0] = str(chr(ord(w[0]) - 32))
-        print(''.join(w))
-    else:
-        print(''.join(w))
+    word = str(input())
+    print(word[0].upper()+word[1:])
+    # upper()함수와 슬라이싱을 사용하여 첫 글자만 대문자로 하고,나머진 그대로 출력함
