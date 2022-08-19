b3ba4e29892178276523094f2e7317020f8fb08c
송병훈 <thdqudgns9612@gmail.com>
2022-08-19 23:06:09 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,20 +1,25 @@
 import sys
+
 a = []
+
 for i in range(9):
     a.append(int(sys.stdin.readline()))
-two_false = sum(a) - 100
+
+two_false = sum(a) - 100    # 전체 합에서 100을 빼어, 가짜 난쟁이 둘의 키의 합을 이용한다.
+
 for i in range(8):
     for j in range(i+1,9):
-        if a[i] + a[j] == two_false:
-            exit_1 = a[i]
-            exit_2 = a[j]
-            a.remove(exit_1)
+        if a[i] + a[j] == two_false:    # 모든 경우의 수에서, 가짜난쟁이 키의 합을 찾으면
+            exit_1, exit_2 = a[i], a[j]
+            a.remove(exit_1)            # 각 값을 저장하고, remove()함수로 제거한다. (중복이 없으므로 가능)
             a.remove(exit_2)
-            a.sort()
-            break
+            break                       # break로 빠져나온다.
         else:
             continue
-    if len(a) == 7:
+    if len(a) == 7:                     # 가짜 난쟁이를 찾아서 7명이 남으면 바깥 반복문도 멈춘다.
         break
-for i in a:
+
+a.sort()
+
+for i in a:     # 하나씩 출력함
     print(i)
