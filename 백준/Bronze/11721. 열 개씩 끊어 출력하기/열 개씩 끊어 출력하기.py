7de5c501968e14d04bbe6fddb439a4213a06a1ca
송병훈 <thdqudgns9612@gmail.com>
2022-08-20 19:53:29 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.py
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.py
@@ -1,3 +1,7 @@
-w = input()
-for i in range(len(w)//10 + 1):
-    print(w[i*10:(i+1)*10])
+w = str(input())    # 문자열로 입력받으면 빠른가?
+for i in range(0, len(w), 10):  # 10씩 건너뛰도록 함
+    print(w[i:i+10])
+# print(w[0:10])
+# print(w[10:20])
+# print(w[20:30])
+# print(w[30:40])
