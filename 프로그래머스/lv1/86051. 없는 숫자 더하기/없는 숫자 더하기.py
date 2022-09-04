fe4740a17e38f098026b1baccf803d0b64679b1c
송병훈 <thdqudgns9612@gmail.com>
2022-09-04 17:27:50 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,5 +1,2 @@
 def solution(numbers):
-    answer = -1
-    number = [0,1,2,3,4,5,6,7,8,9]
-    answer = sum([i for i in number if i not in numbers])
-    return answer
+    return 45 - sum(numbers)
