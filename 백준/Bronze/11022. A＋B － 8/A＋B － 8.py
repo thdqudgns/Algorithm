470abf3e8dbd397c9aaa64b17c9fa21e32b7a469
송병훈 <thdqudgns9612@gmail.com>
2022-09-14 19:17:47 +0900

    [Bronze V] Title: A+B - 8, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,7 +1,4 @@
-# "Case #x: A + B = C"
-
-n = int(input())
-
-for i in range(1,n+1):
-    a,b = map(int,input().split())
-    print(f'Case #{i}: {a} + {b} = {a+b}')
+T = int(input())
+for i in range(T):
+    A, B = map(int, input().split())
+    print('Case #%d: %d + %d = %d'%(i+1, A, B, A+B))
