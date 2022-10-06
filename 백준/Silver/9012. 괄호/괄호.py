305195559c0d5db944f503219384fea8acb429a8
송병훈 <thdqudgns9612@gmail.com>
2022-10-06 22:34:44 +0900

    [Silver IV] Title: 괄호, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -2,10 +2,10 @@ import sys
 N = int(sys.stdin.readline())
 for _ in range(N):
     T = sys.stdin.readline()
+    T = T.replace("\n", "")
     if len(T) != 0:
         while "()" in T:
             T = T.replace("()", "")
-            T = T.replace("\n", "")
         if len(T) == 0:
             print("YES")
         else:
