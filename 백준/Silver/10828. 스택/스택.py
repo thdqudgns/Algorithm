db488aeffe0f4f39bdce29b9f45d9b220b879913
송병훈 <thdqudgns9612@gmail.com>
2022-10-06 22:58:53 +0900

    [Silver IV] Title: 스택, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,33 @@
+import sys
+
+def play(com, num):
+    if com == "push":
+        stack.append(num)
+    elif com == "pop":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            pop = stack.pop()
+            print(pop)
+    elif com == "size":
+        print(len(stack))
+    elif com == "empty":
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif com == "top":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+
+N = int(sys.stdin.readline())
+stack = []
+for _ in range(N):
+    arr = list(map(str, sys.stdin.readline().rstrip().split(' ')))
+    com = arr[0]
+    num = 0
+    if len(arr) > 1:
+        num = int(arr[1])
+    play(com, num)
