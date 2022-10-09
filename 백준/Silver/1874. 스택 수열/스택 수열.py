6f847f0b9682e818407d36868a0ec297c7f8261f
송병훈 <thdqudgns9612@gmail.com>
2022-10-09 23:03:52 +0900

    [Silver II] Title: 스택 수열, Time: 240 ms, Memory: 32424 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -2,8 +2,8 @@
 # 임의의 수열이 주어졌을 때, 그 수열을
 # 오름차순 스택에서 뽑아내어 만들 수 있는지 묻는다.
 # 만들 수 있다면 어떤 순서로 push(+)와 pop(-)연산을 수행해야 하는지 출력한다.
-
-n = int(input())
+import sys
+n = int(sys.stdin.readline().strip())
 
 stack = []
 operator_list = []
@@ -12,7 +12,7 @@ end = 0
 
 for i in range(n):
     # 숫자를 입력받는다
-    input_num = int(input())
+    input_num = int(sys.stdin.readline().strip())
 
     # 숫자를 오름차순으로 증가시키면서 stack에 저장한다.
     # 입력받은 수보다, 오름차순으로 증가하고 있는 숫자가 작다면 stack에 넣는다.
@@ -29,6 +29,7 @@ for i in range(n):
     # 뽑아낼 수 없는 상황이라면 종료한다.
     else:
         end = 1
+        break
 
 if end == 1:
     print("NO")
