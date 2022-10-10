4bb64dcb134bc6ac3c17ac09e628358ea31cd5e1
송병훈 <thdqudgns9612@gmail.com>
2022-10-10 23:13:11 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 852 ms, Memory: 118172 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,17 +1,23 @@
-from bisect import bisect_left, bisect_right
+from collections import Counter
 from sys import stdin
 
 n = stdin.readline().rstrip()
-card = list(map(int,stdin.readline().split()))
+card = list(map(int, stdin.readline().split()))
 m = stdin.readline().rstrip()
-test = list(map(int,stdin.readline().split()))
-card.sort()
+test = list(map(int, stdin.readline().split()))
 
-def count_by_range(a, left_value, right_value):
-    right_index = bisect_right(a, right_value)
-    left_index = bisect_left(a, left_value)
-    return right_index - left_index
+# Counter는 리스트를 값으로 주게 되면
+# 해당 원소들이 몇 번 등장했는지 세어 딕셔너리 형태로 반환한다.
+# {원소:개수, ...} 형태다. 개수의 내림차순으로 정렬된다.
+count = Counter(card)
+# Counter({10: 3, 3: 2, -10: 2, 6: 1, 2: 1, 7: 1})
 
-
-for i in range(len(test)):
-    print(count_by_range(card, test[i], test[i]), end=' ')
+# test에서 하나씩 뽑아낸 값(i)이
+# card의 각 원소 개수를 세어 저장한 count에
+# 있는지 확인하여 있다면 그 개수를, 없다면 0을 출력한다
+# 딕셔너리로 저장된 원소(수)를 입력하면 그에 해당하는 값(개수)이 출력된다.
+for i in test:
+    if i in count:
+        print(count[i], end=' ')
+    else:
+        print(0, end=' ')
