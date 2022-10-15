2cebb73c41e01fdfb967bfacec0c389f0da552dd
송병훈 <thdqudgns9612@gmail.com>
2022-10-15 18:12:39 +0900

    [Silver V] Title: 단어 정렬, Time: 100 ms, Memory: 35452 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,20 +1,7 @@
 import sys
 input = sys.stdin.readline
-
-N = int(input().rstrip())
-arr = []
-for i in range(N):
-    w = input().rstrip()
-    l = len(w)
-    arr.append([w, l])
-# print(arr)
-arr = sorted(arr, key=lambda x: (x[1], x[0]))
-# print(arr)
-
-for i in range(len(arr)):
-    try:
-        if arr[i][0] == arr[i+1][0]:
-            continue
-        print(arr[i][0])
-    except:
-        print(arr[i][0])
+arr = [input().rstrip() for i in range(int(input()))]
+arr = list(set(arr))
+arr.sort()
+arr.sort(key=lambda x:len(x))
+[print(i) for i in arr]
