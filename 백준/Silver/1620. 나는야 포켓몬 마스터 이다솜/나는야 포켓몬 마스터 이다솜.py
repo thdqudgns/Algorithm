54846d8ef161cc1955749dd0f03c57060e118391
송병훈 <thdqudgns9612@gmail.com>
2022-10-16 16:30:36 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 312 ms, Memory: 61056 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -4,18 +4,13 @@ arr_num = dict()
 arr_word = dict()
 for i in range(1, N+1):
     inp = sys.stdin.readline().rstrip()
-    arr_num[i] = inp
-    arr_word[inp] = i
+    arr_num[str(i)] = inp
+    arr_word[inp] = str(i)
 # print(arr_num)
 # print(arr_word)
 for _ in range(M):
     inp = sys.stdin.readline().rstrip()
-    try:
-        inp = int(inp)
-    except:
-        inp = inp
-
-    if type(inp) is int:
-        print(arr_num[inp])
-    else:
+    if inp.isalpha():
         print(arr_word[inp])
+    else:
+        print(arr_num[inp])
