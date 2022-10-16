be6b5b88c0c687e0315c4888e2e06e02f808ed56
송병훈 <thdqudgns9612@gmail.com>
2022-10-16 16:08:06 +0900

    [Silver V] Title: 집합, Time: 5584 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,28 @@
+import sys
+M = int(input())
+S = set()
+for i in range(M):
+    arr = list(map(str, sys.stdin.readline().rstrip().split()))
+    try:
+        com, num = arr[0], int(arr[1])
+    except:
+        com = arr[0]
+
+    if com == 'add':
+        S.add(num)
+    if com == 'remove' and num in S:
+        S.remove(num)
+    if com == 'check':
+        if num in S:
+            print(1)
+        else:
+            print(0)
+    if com == 'toggle':
+        if num in S:
+            S.remove(num)
+        else:
+            S.add(num)
+    if com == 'all':
+        S = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
+    if com == 'empty':
+        S.clear()
