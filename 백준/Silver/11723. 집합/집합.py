853737f635f4e219aae88a79bb571a7404771a5a
송병훈 <thdqudgns9612@gmail.com>
2022-10-16 16:10:26 +0900

    [Silver V] Title: 집합, Time: 5556 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,5 +1,5 @@
 import sys
-M = int(input())
+M = int(sys.stdin.readline())
 S = set()
 for i in range(M):
     arr = list(map(str, sys.stdin.readline().rstrip().split()))
@@ -25,4 +25,4 @@ for i in range(M):
     if com == 'all':
         S = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
     if com == 'empty':
-        S.clear()
+        S = set()
