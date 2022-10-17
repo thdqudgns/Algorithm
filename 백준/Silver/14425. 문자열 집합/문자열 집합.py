0334013b8fc6f13ebd66f31c1546e36e6bfde1c6
송병훈 <thdqudgns9612@gmail.com>
2022-10-18 01:10:02 +0900

    [Silver III] Title: 문자열 집합, Time: 900 ms, Memory: 35784 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,10 +1,11 @@
 N, M = map(int, input().split())
-set_n = set()
+dict_n = dict()
 cnt = 0
 for _ in range(N):
-    set_n.add(input())
+    w = input()
+    dict_n[w] = 0
 for _ in range(M):
     w = input()
-    if w in set_n:
+    if w in dict_n:
         cnt += 1
 print(cnt)
