28f8184211ba386263125e7f3df4753bfe300ec5
송병훈 <thdqudgns9612@gmail.com>
2022-10-18 01:55:24 +0900

    [Silver IV] Title: 큐 2, Time: 1848 ms, Memory: 173484 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,30 @@
+from collections import deque
+import sys
+n = int(sys.stdin.readline())
+q = deque([])
+for i in range(n):
+    arr = sys.stdin.readline().rstrip().split()
+    if arr[0] == 'push':
+        q.append(arr[1])
+    elif arr[0] == 'pop':
+        if q:
+            print(q.popleft())
+        else:
+            print(-1)
+    elif arr[0] == 'size':
+        print(len(q))
+    elif arr[0] == 'empty':
+        if q:
+            print(0)
+        else:
+            print(1)
+    elif arr[0] == 'front':
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    elif arr[0] == 'back':
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
