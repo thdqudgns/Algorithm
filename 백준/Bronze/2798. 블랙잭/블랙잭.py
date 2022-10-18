d9897ce276d3156e42719f8fcd92fada5a799df9
송병훈 <thdqudgns9612@gmail.com>
2022-10-18 22:07:23 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 36188 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,18 +1,13 @@
+# https://pacific-ocean.tistory.com/m/207
 import sys
-input = sys.stdin.readline
 
-N, M = map(int, input().rstrip().split())
-arr = list(map(int, input().rstrip().split()))
-hap = 0
-
-for i in range(N):
-    for j in range(i+1,N):          # i 다음부터
-        for k in range (j+1,N):     # j 다음부터
-            if arr[i] + arr[j] + arr[k] > M:    # M보다 크면 pass
-                continue
-            else:
-                hap = max(hap, arr[i] + arr[j] + arr[k])    # 이전의 합과 지금의 합을 비교하여 더 큰 걸로 저장
-
-# 최댓값이 될 때의 카드 3장까지 보여주려면
-# 합을 저장할 때의 arr[i], arr[j], arr[k] 까지 저장하면 된다.
-print(hap)
+n, m = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
+res = []
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            sum = arr[i] + arr[j] + arr[k]
+            if sum <= m:
+                res.append(sum)
+print(max(res))
