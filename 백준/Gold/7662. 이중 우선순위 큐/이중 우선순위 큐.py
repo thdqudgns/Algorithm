b62f74cb1aa5175379a4cc6b54e253e67e520e89
송병훈 <thdqudgns9612@gmail.com>
2022-10-18 22:21:25 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 7760 ms, Memory: 289912 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -0,0 +1,29 @@
+# https://neomindstd.github.io/%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4/boj7662/
+
+import sys; read=sys.stdin.readline
+import heapq
+
+result=[]
+for T in range(int(read())):
+    visited=[False]*1_000_001
+    minH,maxH=[],[]
+    for i in range(int(read())):
+        s=read().split()
+        if s[0]=='I':
+            heapq.heappush(minH,(int(s[1]),i))
+            heapq.heappush(maxH,(-int(s[1]),i))
+            visited[i]=True
+        elif s[1]=='1':
+            while maxH and not visited[maxH[0][1]]:heapq.heappop(maxH)
+            if maxH:
+                visited[maxH[0][1]]=False
+                heapq.heappop(maxH)
+        else:
+            while minH and not visited[minH[0][1]]:heapq.heappop(minH)
+            if minH:
+                visited[minH[0][1]]=False
+                heapq.heappop(minH)
+    while minH and not visited[minH[0][1]]:heapq.heappop(minH)
+    while maxH and not visited[maxH[0][1]]:heapq.heappop(maxH)
+    result.append(f'{-maxH[0][0]} {minH[0][0]}'if maxH and minH else'EMPTY')
+print('\n'.join(result))
