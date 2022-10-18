82fe28a5220d90bf4da961f195b26dd6e9df765a
송병훈 <thdqudgns9612@gmail.com>
2022-10-18 22:09:26 +0900

    [Bronze II] Title: 블랙잭, Time: 112 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -2,11 +2,11 @@ import sys
 
 n, m = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
-res = []
+res = 0
 for i in range(n):
     for j in range(i+1,n):
         for k in range(j+1,n):
             sum = arr[i] + arr[j] + arr[k]
-            if sum <= m:
-                res.append(sum)
-print(max(res))
+            if sum <= m and sum > res:
+                res = sum
+print(res)
