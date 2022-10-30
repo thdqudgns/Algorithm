71459e412f621cb60d3a821f44a1223ae05998fc
송병훈 <thdqudgns9612@gmail.com>
2022-10-30 23:30:28 +0900

    [D3] Title: 제곱 팰린드롬 수, Time: 115 ms, Memory: 53,048 KB -BaekjoonHub

--- SWEA/D3/10570. 제곱 팰린드롬 수/제곱 팰린드롬 수.py
+++ SWEA/D3/10570. 제곱 팰린드롬 수/제곱 팰린드롬 수.py
@@ -1,32 +1,14 @@
-from math import sqrt
-
-def palindrome(n):
-    n = list(str(n))
-    flag = True
-    for p in range(len(n)):
-        if n[p] != n[-(p+1)]:
-            flag = False
-            break
-    return flag
-
 T = int(input())
 
 for i in range(1, T+1):
     a, b = map(int, input().split())
     cnt = 0
     for num in range(a, b+1):
-        x = num
-        y = sqrt(num)
-        # 31이 1000이하 중 최대의 제곱근이다.
-        # sqrt는 실수로 출력되어 31.0 가 최대인데
-        # 문자열로 따지면 길이가 4인 셈이다.
-        # 따라서 길이가 4가 넘어가는 것들은 진짜 실수기에 제외한다.
-        if len(str(y)) > 4:
+        y = num**(1/2)
+        if y != int(y):
             continue
-        else:
-            y = int(y)
-        if palindrome(x):
-            if palindrome(y):
-                cnt += 1
+        y = int(y)
+        if str(num) == str(num)[::-1] and str(y) == str(y)[::-1]:
+            cnt += 1
 
     print('#{} {}'.format(i, cnt))
