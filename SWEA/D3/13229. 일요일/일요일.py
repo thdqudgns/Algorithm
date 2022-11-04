f2304a0b0a20403d42501b90c59e7af2c775e6f8
송병훈 <thdqudgns9612@gmail.com>
2022-11-04 17:28:14 +0900

    [D3] Title: 일요일, Time: 91 ms, Memory: 44,168 KB -BaekjoonHub

---
+++ SWEA/D3/13229. 일요일/일요일.py
@@ -0,0 +1,16 @@
+week = {'MON':6,
+        'TUE':5,
+        'WED':4,
+        'THU':3,
+        'FRI':2,
+        'SAT':1,
+        'SUN':7}
+
+T = int(input())
+for i in range(1, T+1):
+    w = input()
+    try:
+        result = week[w]
+    except:
+        result = -1
+    print(f'#{i} {week[w]}')
