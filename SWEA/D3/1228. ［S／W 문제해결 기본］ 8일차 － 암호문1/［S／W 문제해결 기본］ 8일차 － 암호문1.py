7110e99821e26bdb802e9bedfc2c7a8dbcdf7f6e
송병훈 <thdqudgns9612@gmail.com>
2022-11-04 17:49:51 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 94 ms, Memory: 44,152 KB -BaekjoonHub

--- SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
@@ -3,7 +3,6 @@ for i in range(1, 11):
     originPW = list(map(str, input().split()))
     commandCnt = int(input())
     command = list(map(str, input().split()))
-    commandDivide = []
     j = 0
     while j < len(command):
         if command[j] == 'I':
@@ -12,7 +11,6 @@ for i in range(1, 11):
             add = command[j+3:j+3+y]
             for k in range(y):
                 originPW.insert(x+k, add[k])
-            # print(originPW)
             j = j+3+y
     print(f'#{i}', end=' ')
     [print(originPW[i], end=' ') for i in range(10)]
