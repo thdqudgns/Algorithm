40f4f9e1f0816e477a960163fd969d86c973e20e
송병훈 <thdqudgns9612@gmail.com>
2022-11-04 16:08:09 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,392 ms, Memory: 253,980 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -1,11 +1,19 @@
-t=int(input())
-for _ in range(t):
-  k=int(input())
-  n=list(map(int, input().split()))
-  e=0
-  while k>1:
-    idx=n.index(max(n))
-    e=e-sum(n[:idx])+n[idx]*idx
-    n=n[(idx+1):]
-    k-=idx+1
-  print(f'#{_+1} {e}')
+t = int(input())
+for tc in range(1, t + 1):
+    date = int(input())     # 연속된 날짜
+    priceList = list(map(int, input().split()))     # 가격 list
+    benefit = 0     # 이익
+    while date > 1:
+        # priceList 중에서 가장 큰 수의 index를 저장함
+        idx = priceList.index(max(priceList))
+
+        # priceList[idx]: 가장 큰 수
+        # idx: 가장 큰 수의 인덱스지만, 이전의 값들의 개수와 같다.
+        # sum(priceList[:idx]): 이전의 값들의 총합
+        # 따라서 가장 큰 수 이전까지의 이익을 계산한다.
+        benefit += priceList[idx] * idx - sum(priceList[:idx])
+
+        # 가격 리스트, 날짜 수를 변경
+        priceList = priceList[(idx + 1):]   # 가장 큰 수 이후의 값들로 새로 생성
+        date -= idx + 1
+    print(f'#{tc} {benefit}')
