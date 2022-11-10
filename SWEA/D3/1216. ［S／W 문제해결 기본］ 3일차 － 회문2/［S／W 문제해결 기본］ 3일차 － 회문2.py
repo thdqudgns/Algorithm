f0b5016fbbfc7a9844130904c0020ec00470ddf4
송병훈 <thdqudgns9612@gmail.com>
2022-11-11 00:06:23 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 289 ms, Memory: 65,656 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -1,29 +1,23 @@
-T = 10
-for tc in range(1, T + 1):
-    N = int(input())
-    words = []
-    for _ in range(100):  # 100줄에 걸쳐서 데이터를 입력받음
-        words.append(list(input()))
-    trans_words = list(map(list, zip(*words)))  # 2차원 배열 뒤집기
-    length = 1  # 최소 가로 길이 1
-
-    # 가로 탐색
-    for i in range(100):  # 0번째 줄부터 100번째 줄까지 탐색
-        for j in range(100):  # 0번째 숫자부터 100번째 숫자까지 탐색
-            for k in range(99, -1, -1):  # 긴 길이부터 탐색
-                if words[i][j:k + 1] == words[i][j:k + 1][::-1]:
-                    if length < k - j + 1:
-                        length = k - j + 1
-                        break
-                    break
-    # 세로 탐색
-    for i in range(100):  # 0번째 줄부터 100번째 줄까지 탐색
-        for j in range(100):  # 0번째 숫자부터 100번째 숫자까지 탐색
-            for k in range(99, -1, -1):  # 긴 길이부터 탐색
-                if trans_words[i][j:k + 1] == trans_words[i][j:k + 1][::-1]:
-                    if length < k - j + 1:
-                        length = k - j + 1
-                        break
-                    break
-
-    print("#{} {}".format(tc, length))
+# 1216. [S/W 문제해결 기본] 3일차 - 회문2
+for _ in range(10):     # 10번 반복.
+    i = int(input())    # i번째 임을 의미 1~10 순서로 입력 받는다.
+    m = [[*map(str, input())]for _ in range(100)]   # 100x100 짜리 2차원 배열을 입력 받는다.
+    z = [*map(list, zip(*m))]   # zip()을 이용해 m을 세로로 읽었을 때의 행렬을 새롭게 만든다.
+    a = 1
+    for j in range(100):
+        t = a+1
+        while t-a < 3:
+            for k in range(101-t):
+                r = m[j][k:k+t]
+                c = z[j][k:k+t]
+                p = 1
+                s = 1
+                for q in range(t//2):
+                    if r[q] != r[t-1-q]:
+                        p = 0
+                    if c[q] != c[t-1-q]:
+                        s = 0
+                if p == 1 or s == 1:
+                    a = t
+            t += 1
+    print(f'#{i} {a}')
