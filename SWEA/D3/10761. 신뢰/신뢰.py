a3ceb18b74cc5f3fd3e6261b822987fd92a7f40b
송병훈 <thdqudgns9612@gmail.com>
2022-11-11 00:14:49 +0900

    [D3] Title: 신뢰, Time: 130 ms, Memory: 57,920 KB -BaekjoonHub

--- SWEA/D3/10761. 신뢰/신뢰.py
+++ SWEA/D3/10761. 신뢰/신뢰.py
@@ -1,17 +1,23 @@
-T = int(input())    
- 
-def sol():
-    li = list(input().split())
-    robots = {'O' : [1,0], 'B': [1,0]}
-    ans = 0
-    for i in range(1, len(li)-1, 2):
-        name = li[i]
-        button = int(li[i+1])
-        dist = abs(button - robots[name][0])
-        robot_sec = ans - robots[name][1]
-        ans += 1 if dist <= robot_sec else (dist-robot_sec+1)
-        robots[name] = [button, ans]
-    return ans
- 
-for tc in range(1, T+1):
-    print(f'#{tc} {sol()}')
+for tc in range(1, int(input())+1):
+    arr=list(input().split())
+    n=int(arr.pop(0))
+    cur_B, cur_O = 1, 1
+    time_B, time_O = 0, 0
+    for i in range(n):
+        if arr[i*2]=='B':
+            int_a=int(arr[i*2+1])
+            time_B+=abs(int_a-cur_B)
+            if time_B<=time_O:
+                time_B+=time_O-time_B+1
+            else:
+                time_B+=1
+            cur_B=int_a
+        else:
+            int_a=int(arr[i*2+1])
+            time_O+=abs(int_a-cur_O)
+            if time_O<=time_B:
+                time_O+=time_B-time_O+1
+            else:
+                time_O+=1
+            cur_O=int_a
+    print(f"#{tc} {max(time_B, time_O)}")
