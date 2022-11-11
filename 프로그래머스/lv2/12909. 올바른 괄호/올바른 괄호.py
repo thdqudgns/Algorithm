ae2106992ec17c56f8b851c9472806453f4c39de
송병훈 <thdqudgns9612@gmail.com>
2022-11-11 10:54:35 +0900

    [level 2] Title: 올바른 괄호, Time: 7.31 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,11 +1,13 @@
 def solution(s):
-    # 함수를 완성하세요
-    open_cnt = 0
+    st = list()
     for c in s:
         if c == '(':
-            open_cnt += 1
-        elif c == ')':
-            open_cnt -= 1
-            if open_cnt < 0:
+            st.append(c)
+
+        if c == ')':
+            try:
+                st.pop()
+            except IndexError:
                 return False
-    return open_cnt == 0
+
+    return len(st) == 0
