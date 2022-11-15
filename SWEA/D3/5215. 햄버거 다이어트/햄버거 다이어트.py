32dd26727225907f122c144ca7d9777b9720dbc0
송병훈 <thdqudgns9612@gmail.com>
2022-11-15 17:59:58 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,582 ms, Memory: 60,416 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -5,7 +5,7 @@ for tc in range(1, int(input())+1):
     # L: 제한 칼로리
     N, L = map(int, input().split())
     # arr: 재료에 대한 민기의 맛 점수[0]와 칼로리[1]
-    arr = [[list(map(int, input().split()))] for _ in range(N)]
+    arr = [list(map(int, input().split())) for _ in range(N)]
     # arr = sorted(arr, key=lambda x : x[0], reverse=True)
     # print(arr)
 
@@ -18,8 +18,8 @@ for tc in range(1, int(input())+1):
             testSumTaste = 0
             # print(values)
             for j in range(len(values)):
-                testSumKcal += values[j][0][1]
-                testSumTaste += values[j][0][0]
+                testSumKcal += values[j][1]
+                testSumTaste += values[j][0]
             if testSumKcal <= L and testSumTaste > maxScore:
                 maxScore = testSumTaste
 
