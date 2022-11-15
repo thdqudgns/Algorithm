1da1b15248ece0a028bc21f8525ac8ad973217d0
송병훈 <thdqudgns9612@gmail.com>
2022-11-15 20:48:17 +0900

    [D3] Title: 세가지 합 구하기, Time: 186 ms, Memory: 61,700 KB -BaekjoonHub

--- SWEA/D3/3408. 세가지 합 구하기/세가지 합 구하기.py
+++ SWEA/D3/3408. 세가지 합 구하기/세가지 합 구하기.py
@@ -1,7 +1,10 @@
+ans = []
 t = int(input())
 for tc in range(1, t+1):
     n = int(input())
     S1 = n*(n+1)//2
     S2 = n**2
     S3 = S1*2
-    print(f'#{tc} {S1} {S2} {S3}')
+    ans.append(f'#{tc} {S1} {S2} {S3}')
+
+[print(i) for i in ans]
