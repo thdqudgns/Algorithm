130ea81a0dd65cdfcb6eb471debf65b207cf27ba
송병훈 <thdqudgns9612@gmail.com>
2022-11-15 17:08:21 +0900

    [D3] Title: 최장 경로, Time: 154 ms, Memory: 60,584 KB -BaekjoonHub

--- SWEA/D3/2814. 최장 경로/최장 경로.py
+++ SWEA/D3/2814. 최장 경로/최장 경로.py
@@ -1,47 +1,26 @@
-def getLength(connection, sn, s, stack):
-    global answer
- 
-    recurse = False
-    for i in range(N):
-        if connection[s][i] and i not in stack :
-            # print(sn, s, i)
-            recurse = True
-            connection[s][i] = False
-            connection[i][s] = False
-            getLength(connection, sn, i, stack + [i])
-            connection[i][s] = True
-            connection[s][i] = True
- 
-    if not recurse:
-        answer = max(len(stack), answer)
- 
- 
-def deep_copy(ref):
-    copied = [[False for _ in range(N)] for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            copied[i][j] = ref[i][j]
-         
-    return copied
- 
- 
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
+def dfs(i, num):
+    global s
+    visit[i] = 1
+    s = max(num, s)
+    for j in arr[i]:
+        if visit[j] == 0:
+            dfs(j, num + 1)
+            visit[j] = 0
+ 
+for tc in range(1, T + 1):
     N, M = map(int, input().split())
  
-    connection = [[False for _ in range(N)] for _ in range(N)]
- 
-    for _ in range(M):
+    arr = [[] for _ in range(N + 1)]
+    for i in range(M):
         x, y = map(int, input().split())
-        connection[x-1][y-1] = True
-        connection[y-1][x-1] = True
-     
-    answer = 0
-    tmp_connection = deep_copy(connection)
-    for i in range(N):
-        length = getLength(connection, i, i, [i])
-        # answer = max(answer, length)
-        connection = deep_copy(tmp_connection)
-     
-    print(f"#{test_case} {answer}")
+        arr[x].append(y)
+        arr[y].append(x)
+ 
+    s = 0
+    for i in range(1, N + 1):
+        visit = [0] * (N + 1)
+        dfs(i, 1)
+ 
+ 
+    print(f'#{tc} {s}')
