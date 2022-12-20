ab1a723e5ec90c19cde8ff00742c3f83aef4b879
송병훈 <thdqudgns9612@gmail.com>
2022-12-20 19:17:33 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 52 ms, Memory: 30748 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,25 +1,21 @@
+import sys
 # n, m 을 입력 받는다
-n, m = map(int, input().split())
+n, m = map(int, sys.stdin.readline().rstrip().split())
 
-# n, m 크기의 행렬을 두 개 만들어 0으로 채운다
-A = [[0 for i in range(m)] for j in range(n)]
-B = [[0 for i in range(m)] for j in range(n)]
-C = [[0 for i in range(m)] for j in range(n)]
+# n, m 크기의 행렬을 만들어 0으로 채운다
+C = [[0 for _ in range(m)] for _ in range(n)]
 
-# 첫번째 행렬 A를 입력받는다
+# 첫번째 행렬 A를 입력받아 C에 더한다
 for i in range(n):
-    A[i] = list(map(int, input().split()))
-    # print(A[i])
-
-# 두번째 행렬 B를 입력받는다.
-for i in range(n):
-    B[i] = list(map(int, input().split()))
-    # print(B[i])
-
-# 더한다.
-for i in range(n):
+    tmp = list(map(int, sys.stdin.readline().rstrip().split()))
     for j in range(m):
-        C[i][j] = A[i][j] + B[i][j]
+        C[i][j] += tmp[j]
+
+# 두번째 행렬 B를 입력받아 C에 더한다.
+for i in range(n):
+    tmp = list(map(int, sys.stdin.readline().rstrip().split()))
+    for j in range(m):
+        C[i][j] += tmp[j]
 
 # 출력한다.
 for i in range(n):
