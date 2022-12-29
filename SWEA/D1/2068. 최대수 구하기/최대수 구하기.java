b24c191244e9a0d563bc242d32399f37f1af51c3
송병훈 <thdqudgns9612@gmail.com>
2022-12-30 00:25:50 +0900

    [D1] Title: 최대수 구하기, Time: 130 ms, Memory: 20,576 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -1,32 +1,33 @@
-import java.util.*;
-import java.io.FileInputStream;
- 
-class Solution
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
 {
-    public static void main(String args[]) throws Exception
-    {
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
- 
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-             
-            ArrayList<Integer> a = new ArrayList();
-            
-            for (int tmp = 10; tmp>0; tmp--) {
-                a.add(sc.nextInt());
+	int test_case;
+	int T;
+	
+	cin>>T;
+    
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+		int answer = 0;
+        
+        // 가장 큰 값이 들어오면 바꿔주는 방식
+        for(int i = 0; i<10; i++){
+        	int tmp;
+            cin >> tmp;
+            if (tmp>answer) {
+            	answer = tmp;
             }
-             
-            Collections.sort(a);
-         
-            //System.out.print('#');
-            //System.out.print(test_case);
-            //System.out.print(" ");
-            //System.out.println(a.get(a.size() - 1));
-             
-            System.out.printf("#%d %d\n",test_case, a.get(a.size() - 1));
- 
-        }
-    }
+        } 
+        
+		cout << '#' << test_case << " " << answer << '\n';
+
+
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
