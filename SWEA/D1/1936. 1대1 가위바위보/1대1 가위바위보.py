a7e291727965e8d5d7a32f213eda18ec2bfbbf3e
송병훈 <thdqudgns9612@gmail.com>
2022-12-29 20:02:56 +0900

    [D1] Title: 1대1 가위바위보, Time: 89 ms, Memory: 43,252 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
@@ -1,6 +1,9 @@
+# 가위는 1, 바위는 2, 보는 3으로 표현되며 A와 B가 무엇을 냈는지 입력으로 주어진다.
+# A와 B중에 누가 이겼는지 판별해보자. 단, 비기는 경우는 없다.
+ 
 a, b = map(int, input().split())
-# 2 > 1 > 3 > 2
-if (a == 2 and b == 1) or (a == 1 and b == 3) or (a == 3 and b == 2):
-    print('A')
-else:
+ 
+if b-a == 1 or b-a == -2:
     print('B')
+else:
+    print('A')
