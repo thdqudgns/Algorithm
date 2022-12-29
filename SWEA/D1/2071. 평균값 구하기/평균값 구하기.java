9a1acf750af66ec443509993db82c9078cbf5090
송병훈 <thdqudgns9612@gmail.com>
2022-12-30 00:36:52 +0900

    [D1] Title: 평균값 구하기, Time: 141 ms, Memory: 20,372 KB -BaekjoonHub

--- SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
@@ -1,24 +1,26 @@
-import java.util.*;
-import java.io.FileInputStream;
- 
-class Solution
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(int argc, char** argv)
 {
-    public static void main(String args[]) throws Exception
-    {
-         
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
- 
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            int result = 0;
-            for (int i = 10; i > 0; i--) result += sc.nextInt();
-             
-            //Math.round를 사용하여 반올림 할 때, 나누는 수가 10이면 반올림이 안되고, 10.0으로 실수형으로 나눠야 반올림이 먹힌다.
-            //왜냐하면 result/10은 바로 정수로 나와서, 28.6이 아니라 28로 나오기 때문이다.
-            System.out.printf("#%d %d\n", test_case, Math.round(result/10.0));
- 
+	int test_case;
+	int T;
+	
+	cin>>T;
+	
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+		int sum = 0;
+
+        for(int i = 0; i < 10; i++) {
+        	int tmp;
+            cin >> tmp;
+            sum += tmp;
         }
-    }
+		
+        // 반올림을 하려면 <cmath> 헤더파일을 추가해야 한다.
+        cout << '#' << test_case << ' ' << round(sum/10.0) << '\n';
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
