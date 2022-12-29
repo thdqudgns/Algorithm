ba89ca4ec152c11f814f790584272b816e2e9232
송병훈 <thdqudgns9612@gmail.com>
2022-12-29 23:58:32 +0900

    [D1] Title: 자릿수 더하기, Time: 88 ms, Memory: 41,128 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
@@ -1,5 +1,51 @@
-a = input()
-result = 0
-for i in a:
-    result += int(i)
-print(result)
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// float b, c;
+// double d, e, f;
+// char g;
+// char var[256];
+// long long AB;
+// cin >> a;                            // int 변수 1개 입력받는 예제
+// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
+// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
+// cin >> g;                            // char 변수 1개 입력받는 예제
+// cin >> var;                          // 문자열 1개 입력받는 예제
+// cin >> AB;                           // long long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// float b = 1.0, c = 2.0;               
+// double d = 3.0, e = 0.0; f = 1.0;
+// char g = 'b';
+// char var[256] = "ABCDEFG";
+// long long AB = 12345678901234567L;
+// cout << a;                           // int 변수 1개 출력하는 예제
+// cout << b << " " << c;               // float 변수 2개 출력하는 예제
+// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
+// cout << g;                           // char 변수 1개 출력하는 예제
+// cout << var;                         // 문자열 1개 출력하는 예제
+// cout << AB;                          // long long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int a;
+    cin >> a;
+    int result = 0;
+    
+    while (a!=0) {
+    	result += a%10;
+        a /= 10;
+    }
+    
+    cout << result;
+    
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
