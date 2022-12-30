0d1b6cfc874f681e2dc48e92f3f16299bea9fd94
송병훈 <thdqudgns9612@gmail.com>
2022-12-31 00:03:12 +0900

    [D2] Title: 스도쿠 검증, Time: 105 ms, Memory: 50,600 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,34 +1,40 @@
-# 1974. 스도쿠 검증
-for tc in range(1, int(input())+1):
-    # 1. 배열 입력받기
-    arr = [list(map(int, input().split())) for _ in range(9)]
-    arr2 = list(zip(*arr))
-    flag = 1
-
-    # 2. 검증 - 행, 열
+test_case = int(input())
+answer = [0 for _ in range(test_case)]  # 정답을 저장할 배열
+for t in range(test_case):
+    sdocu = [list(map(int, input().split())) for _ in range(9)]
+    result = 1  # 정상인지 아닌지
+     
+    # 1. 가로세로 비교(곱이나 합을 사용하는 것보다, 숫자를 비교하는 것이 정확하다)
     for i in range(9):
-        setA = set(arr[i])
-        setB = set(arr2[i])
-        if len(setA) != 9 or len(setB) != 9:
-            flag = 0
+        standardW = [0 for _ in range(10)]  # index를 9까지 하여, index 자체로 숫자 비교를 한다.
+        standardH = [0 for _ in range(10)]  # index를 9까지 하여, index 자체로 숫자 비교를 한다.
+        for j in range(9):
+            # W는 행 비교, H는 열 비교
+            if standardW[sdocu[i][j]] == 0: 
+                standardW[sdocu[i][j]] = 1
+            if standardH[sdocu[j][i]] == 0: 
+                standardH[sdocu[j][i]] = 1            
+        if sum(standardW) < 9 or sum(standardH) < 9: 
+            result = 0
             break
-
-    if flag == 0:
-        print(f'#{tc} {flag}')
-        continue
-
-    # 2. 검증 - 3x3 칸
-    for i in range(0, 9, 3):    # 0, 3, 6
-        for j in range(0, 9, 3):    # 0, 3, 6
-            setC = set()
-            for k in range(i, i+3):
-                for l in range(j, j+3):
-                    setC.add(arr[k][l])
-            if len(setC) != 9:
-                flag = 0
+     
+    # 2. 3x3 공간 비교
+    if result == 1:
+        # 시작점
+        position = [[0, 0], [0, 3], [0, 6], [3, 0], [3, 3], [3, 6], [6, 0], [6, 3], [6, 6]]
+        for i, j in position:
+            standard = [0 for _ in range(10)]
+            for x in range(i, i+3):
+                for y in range(j, j+3):
+                    if standard[sdocu[x][y]] == 0:
+                        standard[sdocu[x][y]] = 1
+            if sum(standard) < 9:
+                result = 0
                 break
-
-    if flag == 0:
-        print(f'#{tc} {flag}')
-    else:
-        print(f'#{tc} {flag}')
+     
+    # 결과 저장
+    answer[t] = result
+     
+# 결과 출력
+for t in range(test_case):
+    print(f'#{t+1} {answer[t]}')
