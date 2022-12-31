aa598ca4b09509f5f7df06656bbbc91679ae6cdc
송병훈 <thdqudgns9612@gmail.com>
2022-12-31 20:10:34 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 105 ms, Memory: 50,756 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
@@ -5,14 +5,7 @@ for _ in range(T):
     score_input = list(map(int, input().split()))
     for s in score_input:
         score_cnt[s] += 1
-     
-    max = 0
-    result_arr = []
-     
-    for i in range(101):
-        if score_cnt[i] >= max:
-            max = score_cnt[i]
-            result_arr.append([max, i])
- 
-    result_arr.sort(key=lambda x: (-x[0], -x[1]))
-    print(f'#{test_case} {result_arr[0][1]}')
+	
+    score_cnt.reverse()
+    ans = score_cnt.index(max(score_cnt))
+    print(f'#{test_case} {100-ans}')
