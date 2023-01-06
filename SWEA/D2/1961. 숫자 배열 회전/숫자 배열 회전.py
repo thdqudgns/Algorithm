26350ebae0529cdaf71c0c8795eeda1c77e3dcd6
송병훈 <thdqudgns9612@gmail.com>
2023-01-07 08:06:20 +0900

    [D2] Title: 숫자 배열 회전, Time: 149 ms, Memory: 58,528 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,15 +1,13 @@
-def rotate(arr, n):
-    arr2 = [[0 for _ in range(n)] for i in range(n)]
+T = int(input())
+for t in range(1, T+1):
+    n = int(input())
+    a = [list(input().split()) for _ in range(n)]
+    b = [[0 for _ in range(n)] for _ in range(n) ]
+     
     for i in range(n):
         for j in range(n):
-            arr2[i][j] = arr[n-1-j][i]
-    return arr2
-
-for tc in range(1, int(input())+1):
-    n = int(input())
-    arr = [list(map(str, input().split())) for _ in range(n)]
-    arr90 = rotate(arr, n)
-    arr180 = rotate(arr90, n)
-    arr270 = rotate(arr180, n)
-    print(f'#{tc}')
-    [print(''.join(arr90[i]), ''.join(arr180[i]), ''.join(arr270[i])) for i in range(n)]
+            b[i][j] = a[n-j-1][i]
+                 
+    print(f'#{t}')
+    for i in range(n):
+        print(f"{''.join(b[i])} {''.join(reversed(a[-i-1]))} {''.join(reversed(b[-i-1]))}")
