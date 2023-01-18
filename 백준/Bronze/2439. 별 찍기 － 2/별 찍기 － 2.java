647c159f7a2df0221223953cc91f592c2bee5320
송병훈 <thdqudgns9612@gmail.com>
2023-01-18 22:35:38 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 392 ms, Memory: 18884 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			String space = " ";
+			for (int j = 0; j < n-i; j++) {
+				System.out.print(space);
+			}
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
