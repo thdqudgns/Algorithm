9283244c088b3f0bfc7dd50bc1044564b876ede1
송병훈 <thdqudgns9612@gmail.com>
2023-01-20 23:58:36 +0900

    [Silver III] Title: 소수 구하기, Time: 268 ms, Memory: 20144 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -9,15 +9,21 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		final int start = Integer.parseInt(st.nextToken());
 		final int end = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
 		
+		// 들어온 수의 마지막 범위까지만 배열 생성
+		// index는 각 수를 의미한다.
+		// 2이상의 수 중, false면 소수고 true면 소수 아님
 		boolean[] sosu = new boolean[end+1];
 		
 		for (int i = 2; i <= end; i++) {
-			if (!sosu[i] && i >= start) System.out.println(i);
+			if (!sosu[i] && i >= start) sb.append(i).append('\n');
 			for (int j = i; j <= end; j += i) {
 				sosu[j] = true;
 			}
 		}
+		
+		System.out.print(sb);
 		
 	}
 }
