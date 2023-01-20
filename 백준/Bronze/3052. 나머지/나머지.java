af854bb528d1aa79c695cca575c54ef723685525
송병훈 <thdqudgns9612@gmail.com>
2023-01-21 00:36:36 +0900

    [Bronze II] Title: 나머지, Time: 208 ms, Memory: 17604 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -2,22 +2,13 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		// 입력받자마자 나머지를 저장한다.
-		int[] input = new int[10];
-		// 각 index는 수를 의미하며, 나머지를 index로 보고 1씩 증가 -> 0이 아닌 것들의 개수를 세면 된다.
+		// 각 index는 수를 의미하며, 값은 나머지로 나온 횟수를 의미한다 1씩 증가 
+        // -> 0이 아닌 것들의 개수를 세면 중복을 피해 셀 수 있다.
 		int[] remainder = new int[42];
-		
-		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i < 10; i++) {
-			input[i] = sc.nextInt() % 42;
-			remainder[input[i]]++;
-		}
-		
 		int cnt = 0;
-		for (int i = 0; i < remainder.length; i++) {
-			if (remainder[i] != 0) cnt++;
-		}
-		
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < 10; i++) remainder[sc.nextInt() % 42]++;    // 입력받자마자 나머지를 저장한다.
+		for (int i = 0; i < remainder.length; i++) if (remainder[i] != 0) cnt++;
 		System.out.println(cnt);
 		
 	}
