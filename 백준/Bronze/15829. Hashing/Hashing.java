8ee596c3f3087330941ea2e063a0dfa4baf7b4f4
송병훈 <thdqudgns9612@gmail.com>
2023-01-31 20:49:47 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,7 +1,9 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		/*
 		 * 항의 번호에 해당하는 만큼 특정한 숫자를 거듭제곱
 		 * 곱해준 다음 더하는 것
@@ -10,17 +12,19 @@ public class Main {
 		 * 
 		 * r 31, M 1234567891
 		 */
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		String str = sc.nextLine();
-		long sum = 0L;
-		int r = 1;
-		int M = 1234567891;
-		for (int i = 0; i < str.length(); i++) {
-			sum += (str.charAt(i) - 96) * r;
-			r *= 31;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		long[] r = new long[n];
+		r[0] = 1;
+		for (int i = 1; i < n; i++) {
+			r[i] = r[i - 1] * 31;
 		}
-		
+		String str = br.readLine();
+		long sum = 0L;
+		int M = 1234567891;
+		for (int i = 0; i < n; i++) {
+			sum += (str.charAt(i) - 96) * r[i];
+		}
 		System.out.println(sum % M);
 	}
 }
