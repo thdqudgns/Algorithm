2eba31db679abac6c993a8ae0d0ce1b3820b8042
송병훈 <thdqudgns9612@gmail.com>
2023-02-01 23:10:56 +0900

    [Silver IV] Title: 수 찾기, Time: 740 ms, Memory: 52084 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/README.md
+++ 백준/Silver/1920. 수 찾기/README.md
@@ -4,11 +4,11 @@
 
 ### 성능 요약
 
-메모리: 49784 KB, 시간: 188 ms
+메모리: 52084 KB, 시간: 740 ms
 
 ### 분류
 
-이분 탐색(binary_search), 자료 구조(data_structures)
+이분 탐색(binary_search), 자료 구조(data_structures), 정렬(sorting)
 
 ### 문제 설명
 
---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		Set<Integer> arr = new TreeSet<>();	// 입력받자마자 정렬
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			arr.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		List<Integer> list = List.copyOf(arr);
+		
+		int M = Integer.parseInt(br.readLine());
+		//int[] find = new int[M];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < M; i++) {
+			//find[i] = Integer.parseInt(st.nextToken());
+			sb.append(binarySearch(list, Integer.parseInt(st.nextToken()))).append('\n');
+		}
+		
+		bw.write(sb.toString());
+		bw.close();
+		
+		
+		//list.forEach(System.out::print);
+		/*
+		for (int i : find) {
+			System.out.println(i);
+		}
+		*/
+	}
+	
+	public static int binarySearch(List<Integer> list, int target) {
+		int start = 0;
+		int end = list.size() - 1;
+		
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (list.get(mid) == target) return 1;
+			
+			if (list.get(mid) > target) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+		
+		return 0;
+		
+	}
+}
