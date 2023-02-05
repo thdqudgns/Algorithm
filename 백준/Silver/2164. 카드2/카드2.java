45c2779a463e1c999b2f2cb590eb6595aa6010a2
송병훈 <thdqudgns9612@gmail.com>
2023-02-06 00:09:38 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 45484 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		Queue<Integer> que = new LinkedList<>();
+		for (int i = 1; i <= N; i++) {
+			que.add(i);
+		}
+		
+		while (que.size() != 1) {
+			que.poll();
+			que.add(que.poll());
+		}
+		
+		System.out.println(que.peek());
+		
+	}
+}
