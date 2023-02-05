3e33fb271c6bfee8d874c086b9764eb183ca188b
송병훈 <thdqudgns9612@gmail.com>
2023-02-06 00:41:13 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,31 +1,23 @@
-import java.util.Scanner;
+// 다른 사람의 천재적인 풀이
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = Integer.parseInt(sc.nextLine());
-		int bon = Integer.MAX_VALUE;
-		if (N % 5 == 0) {
-			bon = N / 5;
-		} 
-		if ((N % 5) % 3 == 0) {
-			int tmp = (N / 5) + ((N % 5) / 3);
-			bon = tmp < bon ? tmp : bon;
-		} 
-		if (N % 3 == 0) {
-			int tmp = N / 3;
-			bon = tmp < bon ? tmp : bon;
-		} 
-		for (int i = 0; i < N / 5; i++) {
-			int tmp = N - 5 * (i + 1);
-			if (tmp % 3 == 0) {
-				int res = (i + 1) + (tmp / 3);
-				bon = res < bon ? res : bon;
-			}
-		}
-		if (bon == Integer.MAX_VALUE) {
-			bon = -1;
-		}
-		System.out.println(bon);
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sugar = Integer.parseInt(br.readLine());
+
+        if ( sugar == 4 || sugar == 7 ){
+            System.out.println(-1);
+        } else if ( sugar % 5 == 1 || sugar % 5 == 3 ) {
+            System.out.println((sugar/5) + 1);
+        } else if ( sugar % 5 == 2 || sugar % 5 == 4 ){
+            System.out.println((sugar/5) + 2);
+        } else if( sugar % 5 == 0 ){
+            System.out.println(sugar/5);
+        }
+    }
 }
