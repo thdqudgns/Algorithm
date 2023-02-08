a0247ea97dd1ab8d131c56f8b60213cb80eb1d70
송병훈 <thdqudgns9612@gmail.com>
2023-02-08 10:31:13 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 121 ms, Memory: 21,960 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,35 +1,45 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Solution {
-	static int[] arr;
-	static int cnt;
-	static Scanner sc = new Scanner(System.in);
+	static int[] board;
+	static int dump, test_case;
+	static StringBuilder sb = new StringBuilder();
+	static final int MAX = 100;
 	
-	public static void main(String[] args) {
-		for(int tc=1;tc<=10;++tc) {
-			arr = new int[101];
-			cnt = sc.nextInt();
-			
-			int high = 0, low = 987654321;
-			
-			for(int i=0;i<100;++i) {
-				int input = sc.nextInt();
-				arr[input]++;
-				high = Math.max(high, input);
-				low = Math.min(low, input);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		board = new int[MAX+1];
+
+		for (int t = 1; t <= 10; t++) {
+			test_case = t;
+			dump = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			// 입력받기
+			for (int i = 1; i < MAX+1; i++) {
+					board[i] = Integer.parseInt(st.nextToken());
 			}
-			
-			for(int i=0;i<cnt;++i) {
-				if(high >= low) {
-					arr[high]--;
-					arr[high-1]++;
-					arr[low]--;
-					arr[low+1]++;
-					while(arr[high] == 0) high--;
-					while(arr[low]==0) low++;
-				}
-			}
-			System.out.println("#" + tc + " " + (high-low));
+			//정렬하기
+			Arrays.sort(board);
+			//dump 횟수만큼 숫자 +- 작업하기
+			work();
 		}
+		
+		System.out.println(sb);
+	}
+	
+	private static void work() {
+		if (dump <= 0 || board[MAX] - board[1] <= 1) {
+			sb.append("#" + test_case + " ").append(board[MAX] - board[1]).append('\n');
+			return;
+		}
+		dump--;
+		board[MAX]--; 
+		board[1]++;
+		Arrays.sort(board);
+		work();	// 재귀호출
 	}
 }
