43569aee23c8098496a51fee863b97129310e989
송병훈 <thdqudgns9612@gmail.com>
2023-02-08 17:56:39 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 856 ms, Memory: 116592 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -15,7 +15,7 @@ public class Main {
 		for (int i = 1; i < N + 1; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j < N + 1; j++) {
-				map[i][j] = map[i][j-1] + Integer.parseInt(st.nextToken());
+				map[i][j] = map[i][j-1] + map[i-1][j] + Integer.parseInt(st.nextToken()) - map[i-1][j-1];
 			}
 		}
 		
@@ -25,10 +25,7 @@ public class Main {
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
-			int sum = 0;
-			for (int x = x1; x <= x2; x++) {
-				sum += map[x][y2] - map[x][y1-1];
-			}
+			int sum = map[x2][y2] - map[x1-1][y2] - map[x2][y1-1] + map[x1-1][y1-1];
 			sb.append(sum).append('\n');
 		}
 		System.out.println(sb);
