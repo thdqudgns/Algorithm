358eabcab0c2fc6022eb61a6a78761ab1581f896
송병훈 <thdqudgns9612@gmail.com>
2023-02-08 17:38:31 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1300 ms, Memory: 118032 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(st.nextToken());	// map 크기
+		int M = Integer.parseInt(st.nextToken());	// test_case
+		
+		int[][] map = new int[N + 1][N + 1];
+		for (int i = 1; i < N + 1; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j < N + 1; j++) {
+				map[i][j] = map[i][j-1] + Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			int sum = 0;
+			for (int x = x1; x <= x2; x++) {
+				sum += map[x][y2] - map[x][y1-1];
+			}
+			sb.append(sum).append('\n');
+		}
+		System.out.println(sb);
+	}
+}
