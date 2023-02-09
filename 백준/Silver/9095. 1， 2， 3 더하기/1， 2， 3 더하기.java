de6e8669a21a8a8623880939eee74f1df8e2eb6d
송병훈 <thdqudgns9612@gmail.com>
2023-02-09 23:14:36 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 140 ms, Memory: 14544 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		// dp 설정하기
+		int[] dp = new int[11];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		for (int i = 4; i < 11; i++) {
+			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+		}
+		// 값 출력하기
+		for (int i = 0; i < T; i++) {
+			int N = Integer.parseInt(br.readLine());
+			sb.append(dp[N] + "\n");
+		}
+		System.out.println(sb);
+	}
+}
