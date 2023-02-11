124fdb3af6ebb6a1f69b0762d701f3639236359f
송병훈 <thdqudgns9612@gmail.com>
2023-02-11 22:33:28 +0900

    [Silver I] Title: 그림, Time: 408 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M;	// map의 행과 열
-	static int cnt = 0, maxArea = 0, area;	// 그림의 개수, 가장 넓은 영역
+	static int cnt = 0, maxArea = 0, area;	// 그림의 개수, 가장 넓은 영역, bfs마다 영역검토
 	static int[][] map;	// map
 	static boolean[][] visited; // 방문여부
 	static int[] dx = {-1, 0, 1, 0};	// 사방탐색
@@ -40,7 +40,7 @@ public class Main {
 				q.offer(new int[]{i, j});
 				visited[i][j] = true;
 				cnt++;
-				area = 1;	// 시작점부터 넓이 포함
+				area = 0;
 				bfs();	// 여기서는 방문처리와, 최대넓이 확인만 하자
 			}
 		}
@@ -50,6 +50,7 @@ public class Main {
 	
 	private static void bfs() {
 		while(!q.isEmpty()) {
+			area++;	// 시작점부터 넓이 포함, q가 비어있지 않다는 것 == 해당 영역이 미방문한 1이라는 뜻
 			int[] position = q.poll();
 			for (int dir = 0; dir < 4; dir++) {
 				int nx = position[0] + dx[dir];
@@ -57,7 +58,6 @@ public class Main {
 				if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
 				if (visited[nx][ny] || map[nx][ny] != 1) continue;
 				visited[nx][ny] = true;
-				area++;
 				q.offer(new int[]{nx, ny});
 			}
 		}
