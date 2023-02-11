223f8f4b7e1e666b73aca66414c3082c988f2b40
송병훈 <thdqudgns9612@gmail.com>
2023-02-12 00:21:20 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -8,7 +8,6 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int N, M;	// map의 행과 열
-	static int cnt = 0, maxArea = 0, area;	// 그림의 개수, 가장 넓은 영역, bfs마다 영역검토
 	static int[][] map;	// map
 	static int[][] visited; // 방문여부
 	static int[] dx = {-1, 0, 1, 0};	// 사방탐색
@@ -24,10 +23,12 @@ public class Main {
 		map = new int[N][M];
 		visited = new int[N][M];
 		
+		// 방문여부 2차원 배열을 원점에서의 거리를 확인하는 용도로 쓴다. 그래서 int[][]형으로
 		for (int i = 0; i < N; i++) {
 			Arrays.fill(visited[i], -1);
 		}
 		
+		// 공백없이 입력되므로, String 클래스의 charAt(j)과 - '0'을 사용함
 		for (int i = 0; i < N; i++) {
 			String S = br.readLine();
 			for (int j = 0; j < M; j++) {
@@ -37,11 +38,16 @@ public class Main {
 		
 		// 2. BFS 활용을 위한 설정
 		q = new ArrayDeque<>();
+		q.offer(new int[]{0, 0}); // 시작점이 (0, 0)이다.
+		visited[0][0] = 0;	// 시작점은 거리가 0 
+		// -> 근데 시작점도 거리에 포함한다고 했으므로 종점을 출력할 때 +1 해준다.
 		
-		q.offer(new int[]{0, 0});
-		visited[0][0] = 0;
-		bfs();	// 여기서는 방문처리와, 최대넓이 확인만 하자
+		bfs();	// 여기서는 방문처리하면서 거리 증가시킨다.
 		System.out.println(visited[N-1][M-1]+1);
+		// BFS 특성상, queue에 들어간 위치 순서대로 탐색을 하므로
+		// 가장 먼저 방문처리를 하면 저절로 최솟값이 저장된다.
+		// 그래서 최솟값을 비교해줄 필요가 없다.
+		
 	}
 	
 	private static void bfs() {
