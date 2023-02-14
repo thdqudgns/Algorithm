a94a6c05a62a5e2abc1a46f309a3226b898774b8
송병훈 <thdqudgns9612@gmail.com>
2023-02-14 13:44:02 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 184 ms, Memory: 29,832 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -6,13 +6,13 @@ import java.util.StringTokenizer;
 
 public class Solution {
 	static int N, M, max;
-	static int[] snack, result;
+	static int[] snack;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		
-		for (int i = 1; i <= T; i++) {
+		for (int t = 1; t <= T; t++) {
 			max = -1;
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			// 주어지는 과자 개수
@@ -20,27 +20,21 @@ public class Solution {
 			// 무게 리미트
 			M = Integer.parseInt(st.nextToken());
 			snack = new int[N];
-			result = new int[2];
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				snack[j] = Integer.parseInt(st.nextToken());
 			}
-			solve(0, 0);
-			sb.append("#" + i + " ").append(max + "\n");
-		} // test_case
-        System.out.println(sb);
-	}
-	private static void solve(int cnt, int start) {
-		if (cnt == 2) {
-			int sum = Arrays.stream(result).sum();
-			if (sum <= M) {
-				max = Math.max(sum, max);
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = i + 1; j < N; j++) {
+					int sum = snack[i] + snack[j];
+					if(sum <= M)
+						max = Math.max(sum, max);
+				}
 			}
-			return;
-		}
-		for (int i = start; i < N; i++) {
-			result[cnt] = snack[i];
-			solve(cnt + 1, i + 1);
-		}
+			
+			sb.append("#" + t + " ").append(max + "\n");
+		} // test_case
+		System.out.println(sb);
 	}
 }
