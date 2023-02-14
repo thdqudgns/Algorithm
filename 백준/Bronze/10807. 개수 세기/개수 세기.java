9b8dd39d9d24bee3098bfaeeec9ccc897aa879d2
송병훈 <thdqudgns9612@gmail.com>
2023-02-14 21:15:47 +0900

    [Bronze V] Title: 개수 세기, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[201];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) arr[Integer.parseInt(st.nextToken())+100]++;
+		int s = Integer.parseInt(br.readLine());
+		System.out.println(arr[s+100]);
+	}
+}
