e467c1fc8b14606374ca9fb75d486496dfd0915a
송병훈 <thdqudgns9612@gmail.com>
2023-02-16 12:45:40 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 524 ms, Memory: 117004 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,46 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N, T, cnt;
-	static List<List<Integer>> list;
+	static int[][] map;
 	static boolean[] visit;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		T = Integer.parseInt(st.nextToken());
-		list = new ArrayList<>();
+		map = new int[N+1][N+1];
 		visit = new boolean[N+1];
-		visit[0] = true;
-		for (int i = 0; i <= N; i++) {
-			list.add(new ArrayList<>());
-		}
 		for (int t = 0; t < T; t++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			list.get(x).add(y);
-			list.get(y).add(x);
+			map[x][y] = 1;
+			map[y][x] = 1;
 		}
-		
 		for (int i = 1; i <= N; i++) {
-			if (visit[i]) continue;
-			dfs(i);
-			cnt++;
+			if(!visit[i]) {
+				dfs(i);
+				cnt++;
+			}
 		}
-		System.out.println(cnt);	// 1번 컴퓨터 제외
+		System.out.println(cnt);
 	}
 	private static void dfs(int n) {
-		if (visit[n]) return;
-		visit[n] = true;
-		for (int i = 0; i < list.get(n).size(); i++) {
-			if (visit[list.get(n).get(i)]) continue;
-			dfs(list.get(n).get(i));
+		for (int i = 1; i <= N; i++) {
+			if (visit[i] || map[n][i] != 1 || map[i][n] != 1) continue;
+			visit[i] = true;
+			dfs(i);
 		}
 	}
 }
