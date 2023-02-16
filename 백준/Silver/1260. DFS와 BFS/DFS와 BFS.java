503915fb5cb78d57eaa5be87f60d88f54f9f1a69
송병훈 <thdqudgns9612@gmail.com>
2023-02-16 12:55:59 +0900

    [Silver II] Title: DFS와 BFS, Time: 400 ms, Memory: 24292 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,74 +1,56 @@
-// https://m.blog.naver.com/lm040466/221787478911
-// 내 이전 풀이는 왜 틀린 것인가... 조사할 필요가 있다.
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	// 함수에서 사용할 변수들
-	static int[][] check; // 간선 연결상태
-	static boolean[] checked; // 확인 여부
-	static int N; // 정점개수
-	static int M; // 간선개수
-	static int V; // 시작정점
-
-	public static void main(String[] args) throws IOException {
+	static int N, T, S, cnt;
+	static int[][] map;
+	static boolean[] visit;
+	static Queue<Integer> q;
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		V = Integer.parseInt(st.nextToken());
-
-		check = new int[N+1][N+1]; // 좌표를 그대로 받아들이기 위해 +1해서 선언
-		checked = new boolean[N+1]; // 초기값 False
-
-		// 간선 연결상태 저장
-		for (int i = 0; i < M; i++) {
+		T = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		map = new int[N+1][N+1];
+		visit = new boolean[N+1];
+		for (int t = 0; t < T; t++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			check[x][y] = check[y][x] = 1;
+			map[x][y] = 1;
+			map[y][x] = 1;
 		}
-
-		dfs(V); // dfs호출
-
-		checked = new boolean[1001]; // 확인상태 초기화
-		System.out.println(); // 줄바꿈
-
-		bfs(); // bfs호출
+		visit[S] = true;
+		dfs(S);
+		System.out.println();
+		Arrays.fill(visit, false);
+		q = new ArrayDeque<Integer>();
+		q.offer(S);
+		visit[S] = true;
+		bfs();
 	}
-
-	// 시작점을 변수로 받아 확인, 출력 후 다음 연결점을 찾아 시작점을 변경하여 재호출
-	public static void dfs(int i) {
-		checked[i] = true;
-		System.out.print(i + " ");
-
-		for (int j = 1; j <= N; j++) {
-			if (check[i][j] == 1 && checked[j] == false) {
-				dfs(j);
-			}
+	private static void dfs(int n) {
+		System.out.print(n + " ");
+		for (int i = 1; i <= N; i++) {
+			if (visit[i] || map[n][i] != 1 || map[i][n] != 1) continue;
+			visit[i] = true;
+			dfs(i);
 		}
 	}
-
-	public static void bfs() {
-		Queue<Integer> queue = new LinkedList<Integer>();
-		queue.offer(V); // 시작점도 Queue에 넣어야 함
-		checked[V] = true;
-		System.out.print(V + " ");
-
-		// Queue가 빌 때까지 반복. 방문 정점은 확인, 출력 후 Queue에 넣어 순서대로 확인
-		while (!queue.isEmpty()) {
-			int temp = queue.poll();
-
-			for (int j = 1; j <= N; j++) {
-				if (check[temp][j] == 1 && checked[j] == false) {
-					queue.offer(j);
-					checked[j] = true;
-					System.out.print(j + " ");
-				}
+	private static void bfs() {
+		while(!q.isEmpty()) {
+			int num = q.poll();
+			System.out.print(num + " ");
+			for (int i = 1; i <= N; i++) {
+				if (visit[i] || map[num][i] != 1 || map[i][num] != 1) continue;
+				visit[i] = true;
+				q.offer(i);
 			}
 		}
 	}
