1944d66d30680f6cd0dc7edabcca26265c5a85c0
송병훈 <thdqudgns9612@gmail.com>
2023-02-20 15:50:21 +0900

    [Silver IV] Title: 수 찾기, Time: 608 ms, Memory: 45428 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,62 +1,45 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.List;
-import java.util.Set;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class Main {
+	static int N, M;
+	static int[] A;
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
-		Set<Integer> arr = new TreeSet<>();	// 입력받자마자 정렬
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		N = Integer.parseInt(br.readLine());
+		A = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			arr.add(Integer.parseInt(st.nextToken()));
+			A[i] = Integer.parseInt(st.nextToken());
 		}
+		Arrays.sort(A);
 		
-		List<Integer> list = List.copyOf(arr);
-		
-		int M = Integer.parseInt(br.readLine());
-		//int[] find = new int[M];
-		st = new StringTokenizer(br.readLine(), " ");
+		M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < M; i++) {
-			//find[i] = Integer.parseInt(st.nextToken());
-			sb.append(binarySearch(list, Integer.parseInt(st.nextToken()))).append('\n');
-		}
-		
-		bw.write(sb.toString());
-		bw.close();
-		
-		
-		//list.forEach(System.out::print);
-		/*
-		for (int i : find) {
-			System.out.println(i);
-		}
-		*/
-	}
-	
-	public static int binarySearch(List<Integer> list, int target) {
-		int start = 0;
-		int end = list.size() - 1;
-		
-		while (start <= end) {
-			int mid = (start + end) / 2;
-			if (list.get(mid) == target) return 1;
-			
-			if (list.get(mid) > target) {
-				end = mid - 1;
-			} else {
-				start = mid + 1;
+			int n = Integer.parseInt(st.nextToken());
+			int start = 0;
+			int end = N - 1;
+			boolean exist = false;
+			while(start <= end) {
+				int mid = start + (end - start) / 2;
+				if (A[mid] == n) {
+					exist = true;
+					break;
+				} else if (A[mid] < n) {
+					start = mid + 1;
+				} else {
+					end = mid - 1;
+				}
 			}
+			if (exist) sb.append("1\n");
+			else sb.append("0\n");
 		}
-        return 0;
-		
+		System.out.println(sb);
 	}
 }
