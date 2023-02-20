747c74131400cdb8f97304a38e135d93a846e2f3
송병훈 <thdqudgns9612@gmail.com>
2023-02-20 16:51:37 +0900

    [Silver I] Title: 쿼드트리, Time: 128 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -3,9 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	static StringBuilder sb = new StringBuilder();
 	static int N;
 	static int[][] map;
-	static String res = "";
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
@@ -16,71 +16,32 @@ public class Main {
 				map[i][j] = in.charAt(j) - '0';
 			}
 		}
-		solve(N/2, 0, 0);	// 절반, 시작 row와 col
-		res = res.replace("(0000)", "0");
-		res = res.replace("(1111)", "1");
-		System.out.println(res);
+		solve(0, 0, N);
+		System.out.println(sb);
 	}
-	/*
-	 * r: 기준시작점 ~ 절반 // c: 기준시작점 ~ 절반
-	 * r: 기준시작점 ~ 절반 // c: 절반 ~ 기준종점
-	 * r: 절반 ~ 기준종점 // c: 기준시작점 ~ 절반
-	 * r: 절반 ~ 기준종점 // c: 절반 ~ 기준종점
-	 */
-	private static void solve(int n, int row, int col) {
-		int start = map[row][col];
-		boolean one = true;
-		boolean two = true;
-		boolean three = true;
-		boolean four = true;
-		res += "(";
-		ONE:
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (map[row + i][col + j] != start) {
-					solve(n/2, row, col);
-					one = false;
-					break ONE;
+
+	private static void solve(int r, int c, int n) {
+		int start = map[r][c];	// 시작지점의 숫자를 저장하여 비교할 때 사용함
+		boolean all = true;		// 모두가 같다면, start를 sb에 추가하고 끝낸다.
+		ALL:
+		for (int i = r; i < r+n; i++) {
+			for (int j = c; j < c+n; j++) {
+				if (map[i][j] != start) {
+					all = false;	// n*n이 통일되지 않았다면
+					break ALL;		// 2중 반복문을 멈추고, 4분할 탐색을 실시한다.
 				}
 			}
 		}
-		if (one) res += String.valueOf(start);
-		
-		TWO:
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (map[row + i][col + j + n] != start) {
-					solve(n/2, row, col + n);
-					two = false;
-					break TWO;
-				}
-			}
+		n /= 2;	// 먼저 n을 2로 나누고
+		if (all) {
+			sb.append(start);		// 모두 통일된 숫자라면 start만 저장하고 끝낸다.
+		} else {					// 4분할 탐색을 실시한다.
+			sb.append("(");			// 4분할 탐색을 시작할 때만 괄호를 넣어주면 된다.
+			solve(r, c, n);
+			solve(r, c+n, n);
+			solve(r+n, c, n);
+			solve(r+n, c+n, n);
+			sb.append(")");			// 4분할 탐색이 끝났다면, 괄호를 닫아준다.
 		}
-		if (two) res += String.valueOf(start);
-		
-		THREE:
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (map[row + i + n][col + j] != start) {
-					solve(n/2, row + n, col);
-					three = false;
-					break THREE;
-				}
-			}
-		}
-		if (three) res += String.valueOf(start);
-		
-		FOUR:
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (map[row + i + n][col + j + n] != start) {
-					solve(n/2, row + n, col + n);
-					four = false;
-					break FOUR;
-				}
-			}
-		}
-		if (four) res += String.valueOf(start);
-		res += ")";
 	}
 }
