0b147ae7e3aee06e29817eb2723032a15c6df9cc
송병훈 <thdqudgns9612@gmail.com>
2023-02-22 23:17:30 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
-class Main {
+
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int sum = 0;
-		for(int i=1; i<n+1; i++) sum += i;
-		System.out.println(sum);
+		System.out.println(n*(n+1)/2);
 	}
 }
