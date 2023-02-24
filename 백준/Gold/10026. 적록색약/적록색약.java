292afa764b1ea5efa86393d334f14cc120c754ab
송병훈 <thdqudgns9612@gmail.com>
2023-02-24 22:11:57 +0900

    [Gold V] Title: 적록색약, Time: 168 ms, Memory: 16432 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -4,13 +4,15 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
+// 적록색약: R=G
+// 적록색약이 아닌 사람이 봤을 때의 구역 수, 적록색약인 사람이 봤을 때의 구역 수
 public class Main {
-	static int N, rg, rgb;
-	static char[][] map1;
-	static char[][] map2;
-	static boolean[][] visit;
-	static Queue<int[]> q;
+
+	static char[][] map1, map2;
+	static int N, cnt1, cnt2;
+	static Queue<int[]> q1, q2;
 	static int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
@@ -19,81 +21,64 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			String s = br.readLine();
 			for (int j = 0; j < N; j++) {
-				char c = s.charAt(j);
-				map1[i][j] = c;
-				if (c == 'G') {
+				map1[i][j] = s.charAt(j); 
+				if (map1[i][j] == 'G') {
 					map2[i][j] = 'R';
 				} else {
-					map2[i][j] = c;
+					map2[i][j] = map1[i][j];
 				}
 			}
 		}
-		
-		visit = new boolean[N][N];
+		q1 = new ArrayDeque<>();
+		q2 = new ArrayDeque<>();
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (visit[i][j]) continue;
-				q = new ArrayDeque<>();
-				q.offer(new int[] {i, j});
-				visit[i][j] = true;
-				rgb++;
-				bfs1();
+				if (map1[i][j] != '0') {
+					q1.offer(new int[] {i, j});
+					bfs1();
+					cnt1++;
+				}
+				if (map2[i][j] != '0') {
+					q2.offer(new int[] {i, j});
+					bfs2();
+					cnt2++;
+				}
 			}
 		}
-		
-		visit = new boolean[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (visit[i][j]) continue;
-				q = new ArrayDeque<>();
-				q.offer(new int[] {i, j});
-				visit[i][j] = true;
-				rg++;
-				bfs2();
-			}
-		}
-		
-		System.out.println(rgb + " " + rg);
+		System.out.println(cnt1 + " " + cnt2);
 	}
-	
-	// 정상인
+
 	private static void bfs1() {
-		int[] s = q.peek();
-		int a = s[0];
-		int b = s[1];
-		char start = map1[a][b];
-		while (!q.isEmpty()) {
-			int[] out = q.poll();
-			int x = out[0];
-			int y = out[1];
+		char start = map1[q1.peek()[0]][q1.peek()[1]];
+		map1[q1.peek()[0]][q1.peek()[1]] = '0';
+		while(!q1.isEmpty()) {
+			int[] tmp = q1.poll();
+			int x = tmp[0];
+			int y = tmp[1];
 			for (int i = 0; i < 4; i++) {
 				int nx = x + dx[i];
 				int ny = y + dy[i];
-				if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-				if (visit[nx][ny] || map1[nx][ny] != start) continue;
-				visit[nx][ny] = true;
-				q.offer(new int[] {nx, ny});
+				if (nx < 0 || nx>=N || ny < 0 || ny>=N) continue;
+				if (map1[nx][ny] != start) continue;
+				map1[nx][ny] = '0';
+				q1.offer(new int[] {nx, ny});
 			}
 		}
 	}
-	
-	// 적록색약인
 	private static void bfs2() {
-		int[] s = q.peek();
-		int a = s[0];
-		int b = s[1];
-		char start = map2[a][b];
-		while (!q.isEmpty()) {
-			int[] out = q.poll();
-			int x = out[0];
-			int y = out[1];
+		char start = map2[q2.peek()[0]][q2.peek()[1]];
+		map2[q2.peek()[0]][q2.peek()[1]] = '0';
+		while(!q2.isEmpty()) {
+			int[] tmp = q2.poll();
+			int x = tmp[0];
+			int y = tmp[1];
 			for (int i = 0; i < 4; i++) {
 				int nx = x + dx[i];
 				int ny = y + dy[i];
-				if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-				if (visit[nx][ny] || map2[nx][ny] != start) continue;
-				visit[nx][ny] = true;
-				q.offer(new int[] {nx, ny});
+				if (nx < 0 || nx>=N || ny < 0 || ny>=N) continue;
+				if (map2[nx][ny] != start) continue;
+				map2[nx][ny] = '0';
+				q2.offer(new int[] {nx, ny});
 			}
 		}
 	}
