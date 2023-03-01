c9271c5061d5ba2336430c63fc3faabc99561d7d
송병훈 <thdqudgns9612@gmail.com>
2023-03-01 23:36:28 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 364 ms, Memory: 19580 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,5 +1,4 @@
-import java.util.ArrayDeque;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
@@ -8,7 +7,7 @@ public class Main {
 		int K = sc.nextInt();
 		StringBuilder sb = new StringBuilder();
 		sb.append("<");
-		ArrayDeque<Integer> dq = new ArrayDeque<>();
+		Queue<Integer> dq = new ArrayDeque<>();
 		for (int i = 1; i <= N; i++) dq.offer(i);
 		int cnt = 1;
 		while (!dq.isEmpty()) {
