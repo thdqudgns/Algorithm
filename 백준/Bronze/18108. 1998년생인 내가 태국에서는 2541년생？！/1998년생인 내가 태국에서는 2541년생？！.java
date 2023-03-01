e49cf186e6c0c77bc9d59ad45768bb8c54146f7f
송병훈 <thdqudgns9612@gmail.com>
2023-03-01 22:53:20 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 232 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
-class Main {
+public class Main {
 	public static void main(String[] args) {
-		 Scanner sc = new Scanner(System.in);
-		 int year = sc.nextInt();
-		 System.out.println( year - 543 );
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() - 543);
 	}
 }
