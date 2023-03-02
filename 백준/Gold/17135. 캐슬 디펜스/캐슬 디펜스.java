f49ec719cc5650704ebc76708862f17885694d3a
송병훈 <thdqudgns9612@gmail.com>
2023-03-02 23:15:43 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 388 ms, Memory: 24308 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -1,9 +1,4 @@
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 	static class Enemy {
@@ -20,25 +15,26 @@ public class Main {
 		M = sc.nextInt();
 		D = sc.nextInt();
 		List<Enemy> enemies = new LinkedList<>();
-		// 1. 적들의 위치를 저장
-		for (int i = 0; i < N; i++) for (int j = 0; j < M; j++) if (sc.nextInt() == 1) enemies.add(new Enemy(i, j)); 
 		
-		// 2. 궁수의 위치를 선정(조합)
-		for (int i = 0; i < M - 2; i++) {
-			for (int j = i + 1; j < M - 1; j++) {
-				for (int k = j + 1; k < M; k++) {
-					List<Enemy> copyEnemies = new LinkedList<>();
-					for (Enemy e : enemies) copyEnemies.add(new Enemy(e.r, e.c)); 
-					
-					// 궁수의 위치에서 적들을 모두 처치하자.
-					killEnemies(copyEnemies, new int[] {i, j, k});
-				}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (sc.nextInt() == 1) enemies.add(new Enemy(i, j));
 			}
 		}
 		
+		for (int i = 0; i < M - 2; i++) {
+			for (int j = i+1; j < M - 1; j++) {
+				for (int k = j+1; k < M; k++) {
+					List<Enemy> copyEnemies = new LinkedList<>();
+					for (Enemy e : enemies) copyEnemies.add(new Enemy(e.r, e.c));
+					
+					killEnemies(copyEnemies, new int[] {i,j,k});
+				}
+			}
+		}
 		System.out.println(ans);
 	}
-	static void killEnemies(List<Enemy> copyEnemies, int[] archers) {
+	private static void killEnemies(List<Enemy> copyEnemies, int[] archers) {
 		int cnt = 0;
 		while (!copyEnemies.isEmpty()) {
 			Set<Enemy> removeEnemies = new HashSet<>();
@@ -48,46 +44,40 @@ public class Main {
 					removeEnemies.add(e);
 				}
 			}
-			// 현재 턴에서 찾은 적들의 카운트 세기
 			cnt += removeEnemies.size();
-			// 현재 턴에서 찾은 적들을 리스트에서 제거
 			copyEnemies.removeAll(removeEnemies);
-			// 적들을 하강시키자..
 			downEnemy(copyEnemies);
 		}
-		// 모든 적이 처치 되었다.
 		ans = Math.max(ans, cnt);
 	}
-	static void downEnemy(List<Enemy> copyEnemies) {
-		// 리스트를 반복하면서 적의 r의 자표를 증가시킨다.
-		// 증가시킨 좌표가 N과 같다면 적은 제거한다.
-		Iterator<Enemy> ite = copyEnemies.iterator();
-		while (ite.hasNext()) {
-			Enemy e = ite.next();
-			e.r++;
-			if (e.r == N) ite.remove();
-		}
-	}
-	static Enemy findEnemy(List<Enemy> copyEnemies, int archer) {
+	private static Enemy findEnemy(List<Enemy> copyEnemies, int archer) {
 		int minD = 1 << 30, minC = 50;
 		Enemy find = null;
 		for (Enemy e : copyEnemies) {
 			int d = N - e.r + Math.abs(archer - e.c);
 			if (d > D || d > minD) continue;
 			
-			// 새롭게 찾은 적과의 거리가 기존에 찾은 적과의 거리보다 적은 경우
 			if (d < minD) {
 				minD = d;
 				minC = e.c;
 				find = e;
 				continue;
 			}
-			// 새롭게 찾은 적의 거리가 현재의 최소 거리와 같다.
+			
 			if (minC > e.c) {
 				minC = e.c;
 				find = e;
 			}
 		}
 		return find;
+	}
+	private static void downEnemy(List<Enemy> copyEnemies) {
+		Iterator<Enemy> ite = copyEnemies.iterator();
+		while (ite.hasNext()) {
+			Enemy e = ite.next();
+			e.r++;
+			if (e.r == N) ite.remove();
+		}
+		
 	}
 }
