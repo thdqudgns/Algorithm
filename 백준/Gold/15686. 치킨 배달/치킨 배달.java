671fd6e395f1feb4969e92158d5afc40588817d3
송병훈 <thdqudgns9612@gmail.com>
2023-03-02 13:58:14 +0900

    [Gold V] Title: 치킨 배달, Time: 212 ms, Memory: 18100 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,57 +1,56 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N, M, C, H;
-	static int min = Integer.MAX_VALUE;
+	static int N, M, cityLen = Integer.MAX_VALUE;
 	static int[][] map;
-	static List<int[]> chicken = new ArrayList<>();
-	static int[][] pickChicken;
-	static List<int[]> house = new ArrayList<>();;
+	static List<int[]> homes = new ArrayList<>();;
+	static List<int[]> chickens = new ArrayList<>();;
+	static int[][] pick;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		map = new int[N+1][N+1];	// 1부터 담기 시작함
-		for (int i = 1; i < N+1; i++) {
+		map = new int[N+1][N+1];
+		pick = new int[M][];
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j < N+1; j++) {
+			for (int j = 1; j <= N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == 2) chicken.add(new int[] {i, j}); 
-				if(map[i][j] == 1) house.add(new int[] {i, j}); 
+				if (map[i][j] == 1) homes.add(new int[] {i, j});
+				if (map[i][j] == 2) chickens.add(new int[] {i, j});
 			}
 		}
-		C = chicken.size();
-		H = house.size();
-		pickChicken = new int[C][2];
+		
 		comb(0, 0);
 		
-		System.out.println(min);
+		System.out.println(cityLen);
 	}
-	
 	private static void comb(int cnt, int start) {
 		if (cnt == M) {
-			// 거리측정하기
-			int len = 0;
-			for (int x = 0; x < H; x++) { // 집마다, 치킨집까지의 거리 계산
-				int hL = Integer.MAX_VALUE;
-				for (int y = 0; y < M; y++) {
-					int tmp = 0;
-					tmp += Math.abs(house.get(x)[0] - pickChicken[y][0]);
-					tmp += Math.abs(house.get(x)[1] - pickChicken[y][1]);
-					//System.out.println(tmp);
-					hL = Math.min(tmp, hL);
+			int tmpCityLen = 0;
+			// 치킨거리 구하기
+			for (int[] home : homes) {
+				int chickenLen = Integer.MAX_VALUE;
+				for (int[] chicken : pick) {
+					int distance = distance(home, chicken);
+					chickenLen = Math.min(chickenLen, distance);
 				}
-				len += hL;
+				tmpCityLen += chickenLen;
+                if (tmpCityLen > cityLen) return;
 			}
-			min = Math.min(len, min);
+			cityLen = Math.min(cityLen, tmpCityLen);
 			return;
 		}
 		
-		for (int i = start; i < C; i++) {
-			pickChicken[cnt] = chicken.get(i);
-			comb(cnt + 1, i + 1);
+		for (int i = start; i < chickens.size(); i++) {
+			pick[cnt] = chickens.get(i);
+			comb(cnt+1, i+1);
 		}
+	}
+	
+	private static int distance(int[] home, int[] chicken) {
+		return Math.abs(home[0] - chicken[0]) + Math.abs(home[1] - chicken[1]);
 	}
 }
