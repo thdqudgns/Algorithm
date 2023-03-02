8dfcac51a40508a6b26363f15ec2e4c1e3ddda68
송병훈 <thdqudgns9612@gmail.com>
2023-03-02 12:36:43 +0900

    [Gold IV] Title: 최단경로, Time: 788 ms, Memory: 107704 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -0,0 +1,81 @@
+//package solve;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static class Vertex implements Comparable<Vertex> {
+		int num, cost;
+		public Vertex(int num, int cost) {
+			this.num = num;
+			this.cost = cost;
+		}
+		@Override
+		public int compareTo(Vertex o) {
+			// 가중치(비용)의 오름차순으로 PQ를 정렬할 거다.
+			return Integer.compare(this.cost, o.cost);
+		}
+	}
+	
+	static List<Vertex>[] data;
+	static PriorityQueue<Vertex> pq;
+	static boolean[] visited;
+	static int V, E, ans;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
+		
+		int start = Integer.parseInt(br.readLine())-1;
+		data = new List[V];
+		for (int i = 0; i < V; i++) {
+			data[i] = new ArrayList<Vertex>();
+		}
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			data[from-1].add(new Vertex(to-1, cost));
+		}
+		
+		pq = new PriorityQueue<>();
+		pq.offer(new Vertex(start, 0));
+		
+		final int INF = Integer.MAX_VALUE;
+		int[] distance = new int[V];
+		boolean[] visited = new boolean[V];
+		
+		Arrays.fill(distance, INF);
+		distance[start] = 0;
+		int min, current, cnt = 0;
+		while (!pq.isEmpty()) {
+			Vertex minVt = pq.poll();
+			//if (visited[minVt.num]) continue;
+			current = minVt.num;
+			min = minVt.cost;
+			
+			visited[current] = true;
+			//if (++cnt == V) break;
+			
+			for (int i = 0; i < data[current].size(); i++) {
+				if (!visited[data[current].get(i).num] 
+						&& distance[data[current].get(i).num] > min + data[current].get(i).cost) {
+					distance[data[current].get(i).num] = min + data[current].get(i).cost;
+					pq.offer(new Vertex(data[current].get(i).num, distance[data[current].get(i).num]));
+				}
+			}
+		}
+		for (int i = 0; i < V; i++) {
+			if (distance[i] != INF) {
+				sb.append(distance[i] + "\n");
+			} else {
+				sb.append("INF\n");
+			}
+		}
+		System.out.print(sb);
+	}
+}
