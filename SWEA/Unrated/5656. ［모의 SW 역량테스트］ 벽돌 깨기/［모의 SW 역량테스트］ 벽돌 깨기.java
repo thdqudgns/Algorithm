e1e33436cfda7058c227fd9b99c96d8c623466fe
송병훈 <thdqudgns9612@gmail.com>
2023-03-03 11:15:40 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 406 ms, Memory: 94,656 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -46,52 +46,51 @@ public class Solution {
 			solve(cnt + 1, breaking(i, copyMap));
 		}
 	}
-	private static int[][] breaking(int col, int[][] copyMap) {
+	private static int[][] breaking(int col, int[][] map) {
 		int row = 0;
 		int range = 0;
 		for (int i = 0; i < H; i++) {
-			if (copyMap[i][col] != 0) {
+			if (map[i][col] != 0) {
 				row = i;
-				range = copyMap[i][col];
+				range = map[i][col];
 				break;
 			}
 		}
 		// row, col : 벽돌이 깨지기 시작하는 위치
 		// range : 깨지는 범위
-		breakStone(row, col, range, copyMap);
-		return downStone(copyMap);
+		breakStone(row, col, range, map);
+		return downStone(map);
 	}
-	private static void breakStone(int row, int col, int range, int[][] copyMap) {
+	private static void breakStone(int row, int col, int range, int[][] map) {
 		if (row<0 || row>=H || col<0 || col>=W) return;
-		copyMap[row][col] = 0;
+		map[row][col] = 0;
 		// row가 증가할 때, col은 유지된다.
 		for (int r = row - range + 1; r < row + range; r++) {
-			if (r<0 || r>=H || copyMap[r][col] == 0) continue;
-			if (copyMap[r][col] == 1) copyMap[r][col] = 0;
-			if (copyMap[r][col] > 1) breakStone(r, col, copyMap[r][col], copyMap);
+			if (r<0 || r>=H || map[r][col] == 0) continue;
+			if (map[r][col] == 1) map[r][col] = 0;
+			if (map[r][col] > 1) breakStone(r, col, map[r][col], map);
 		}
 		// col이 변할 때, row는 유지된다.
 		for (int c = col - range + 1; c < col + range; c++) {
 			if (c<0 || c>=W) continue;
-			if (copyMap[row][c] == 1) copyMap[row][c] = 0;
-			if (copyMap[row][c] > 1) breakStone(row, c, copyMap[row][c], copyMap);
+			if (map[row][c] == 1) map[row][c] = 0;
+			if (map[row][c] > 1) breakStone(row, c, map[row][c], map);
 		}
 	}
-	private static int[][] downStone(int[][] copyMap) {
+	private static int[][] downStone(int[][] map) {
 		//열마다 벽돌을 내려준다.
 		for (int w = 0; w < W; w++) {
 			List<Integer> keep = new ArrayList<>();
 			for (int h = 0; h < H; h++) {
-				if (copyMap[h][w] != 0) keep.add(copyMap[h][w]);
-				copyMap[h][w] = 0;
+				if (map[h][w] != 0) keep.add(map[h][w]);
+				map[h][w] = 0;
 			}
 			int h = H-1;
 			int index = keep.size()-1;
-			while (h>=0) {
-				if (index < 0) break;
-				copyMap[h--][w] = keep.get(index--);
+			while (h>=0 && index>=0) {
+				map[h--][w] = keep.get(index--);
 			}
 		}
-		return copyMap;
+		return map;
 	}
 }
