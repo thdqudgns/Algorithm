11954f05f2d2ce833c53d4410b4517ece153e7c6
송병훈 <thdqudgns9612@gmail.com>
2023-03-04 20:05:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벌꿀채취, Time: 200 ms, Memory: 28,272 KB -BaekjoonHub

--- SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
+++ SWEA/Unrated/2115. ［모의 SW 역량테스트］ 벌꿀채취/［모의 SW 역량테스트］ 벌꿀채취.java
@@ -1,65 +1,85 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Solution {
-	static int N, M, C, ans; // 통 크기, 선택할 벌통수, 꿀의 최대 양, 정답
-	static int[][] map, profit;
+	static int N, M, C, ans;
+	static int[][] map, extract; // 꿀 정보를 저장할 배열, 꿀의 
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt(), t = 0;
-		
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine()), t = 0;
+		StringBuilder sb = new StringBuilder();
 		while(t++ < T) {
-			N = sc.nextInt();
-			M = sc.nextInt();
-			C = sc.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			C = Integer.parseInt(st.nextToken());
+			
 			map = new int[N][N];
-			profit = new int[N][N];
+			extract = new int[N][N-M+1];
+			ans = 0;
+			
 			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
-					map[i][j] = sc.nextInt();
+					map[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
-			setProfit();
-			setPosition();
-			System.out.printf("#%d %d\n", t, ans);
-			ans = 0;
+			setExtract();
+			getMaxResult();
+			sb.append(String.format("#%d %d\n", t, ans));
 		}
+		System.out.println(sb);
 	}
 
-	private static void setPosition() {
+	private static void getMaxResult() {
+		List<Integer> idxMaxCash = new ArrayList<>();
 		for (int i = 0; i < N; i++) {
-			for (int j = 0; j <= N - M; j++) {
+			idxMaxCash.add(Arrays.stream(extract[i]).max().orElse(0));
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j <= N-M; j++) {
+				idxMaxCash.set(i, 0);
+				int cur = extract[i][j];
 				
-				// 같은 행일 때
-				for (int c = j + M; c <= N - M; c++) {
-					ans = Math.max(ans, profit[i][j] + profit[i][c]);
+				int rowMax = 0;
+				int nextRowMax = idxMaxCash.stream().mapToInt(Integer::valueOf).max().orElse(0);
+				for (int k = j+M; k <= N-M; k++) {
+					rowMax = Integer.max(rowMax, extract[i][k]);
 				}
-				// 다른 행일 때
-				for (int r = i + 1; r < N; r++) {
-					for (int c = 0; c <= N - M; c++) {
-						ans = Math.max(ans, profit[i][j] + profit[r][c]);
-					}
-				}
+				ans = Math.max(ans, cur + Math.max(rowMax, nextRowMax));
 			}
 		}
 	}
 
-	private static void setProfit() {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j <= N - M; j++) {
-				setMaxProfit(i, j, 0, 0, 0);
-			}
-		}
+	private static void setExtract() {
+		for (int i = 0; i < N; i++) 
+			for (int j = 0; j <= N-M; j++)
+				// (i, j)에서 채집할 수 있는 최대 가격을 저장한다.
+				extract[i][j] = getCost(i, j);
 	}
 
-	private static void setMaxProfit(int i, int j, int cnt, int cSum, int pSum) {
-		if (cSum > C) return;
-		if (cnt == M) {
-			profit[i][j - M] = Math.max(profit[i][j - M], pSum);
-			return;
+	private static int getCost(int x, int y) {
+		int[] cur = new int[M]; // 벌통을 선택하여 임시 저장할 배열
+		System.arraycopy(map[x], y, cur, 0, M); // map[x] 배열의 y열부터 M개의 값들을 -> cur배열의 0 index부터 저장한다.
+		
+		int maxCost = Integer.MIN_VALUE;
+		// M개의 원소들의 부분집합 중에서, 채집 가능한 최댓값을 반환한다.
+		for (int i = 0; i < (1<<M); i++) {
+			// 하나의 부분집합 상황마다
+			int cnt = 0;	// 꿀의 양 저장
+			int square = 0; // 꿀의 가격 저장
+			for (int j = 0; j < M; j++) {
+				if ((i&1<<j) == 0) continue; // 0이면 부분집합에서 제외
+				cnt += cur[j];
+				square += cur[j]*cur[j]; // 가격은 양의 제곱이다.
+			}
+			// cnt와 square에 부분집합별 꿀의 양과 가격이 저장되었다. 이제 그 중에 최대값을 반환하자.
+			if (cnt > C) continue;
+			maxCost = Math.max(maxCost, square);
 		}
-		setMaxProfit(i, j + 1, cnt + 1, cSum, pSum);
-		setMaxProfit(i, j + 1, cnt + 1, cSum + map[i][j], pSum + map[i][j] * map[i][j]);
+		return maxCost;
 	}
 }
