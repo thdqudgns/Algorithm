37fbb8d17aa900013c6070cdb96d179f3c4fdf1b
송병훈 <thdqudgns9612@gmail.com>
2023-03-24 00:37:07 +0900

    [level 2] Title: 문자열 압축, Time: 0.04 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -1,36 +1,33 @@
-/**
-    압축하려면 어차피 하나씩 다 확인해봐야 한다. 
-    압축하는 중에 현재 저장된 최솟값보다 길이가 길어진다면 return하여 백트래킹
-*/
 class Solution {
-    static int answer, count;
-    static StringBuilder result;
     public int solution(String s) {
-        answer = s.length();
-        for(int i = 1; i <= s.length()/2; i++) {
-            solve(s, i); // i를 기준으로 문자열을 잘라서 압축해본다.
-        }
-        return answer;
-    }
-    
-    static void solve(String s, int num) {
-        result = new StringBuilder();
-        String base = s.substring(0, num);
-        count = 1;
-        for(int i = num; i<=s.length(); i += num) {
-            int endIdx = Math.min(i + num, s.length());
-            String compare = s.substring(i, endIdx);
-            if(base.equals(compare)) {
-                count++;
-            } else {
-                if (count >= 2) result.append(count);
-                result.append(base);
-                base = compare;
-                count = 1;
+        int min = s.length();
+        int len = s.length()/2+1;
+        for(int i = 1; i < len; i++) {
+            String before = "";
+            int sum = 0;
+            int cnt = 1;
+            for(int j = 0; j < s.length();) {               
+                int start = j;
+                j = (j+i > s.length()) ? s.length():j+i;
+                String temp = s.substring(start, j);
+                if(temp.equals(before)) {
+                    cnt++;
+                } else {
+                    if(cnt != 1) {
+                        sum += (int)Math.log10(cnt)+1;
+                    }
+                    cnt = 1;
+                    sum+=before.length();
+                    before = temp;
+                }
             }
-            if (result.length() > answer) return;
+            sum+=before.length();
+            if(cnt != 1) {
+                sum += (int)Math.log10(cnt)+1;
+            }
+            min = (min > sum) ? sum : min;
         }
-        result.append(base);
-        answer = Math.min(answer, result.length());
+
+        return min;
     }
 }
