34ad3e9bd6a5b16ccaa0a60dd0d6834927a055d4
송병훈 <thdqudgns9612@gmail.com>
2023-03-28 00:37:04 +0900

    [level 2] Title: 더 맵게, Time: 1822.17 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,38 @@
+import java.util.*;
+/**
+    가장 작은 두 개를 계속 사용하는 문제다.
+    시간복잡도 = (가장 작은 두 수를 찾기 위해 정렬하는 시간) * (배열의 개수 * 2) // 2는 두 수를 골라 연산하는 것
+    
+    1. 배열을 계속 정렬할까? -> 시간초과 날 것 같다. 
+    두 개를 더하는 시간 최대 백만, 백만번 각각 정렬하는 시간 백만log(백만)
+    (2 * 백만) + (백만 * 백만lon백만) = O(백만^2log백만) => 일조 * 19 = 19조... 말도안된다.
+    
+    2. 최소힙을 사용하면 100만을 정렬하는데 log₂백만 ≒ 20으로, 1900만 보다 굉장히 빠르다.
+    시간복잡도 = 20 * 100만 * 2 = 4000만
+    
+    무려 사십칠만오천배 빨라졌다.
+ */
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int answer = 0;
+        
+        // 배열 -> 우선순위큐 (최악 백만)
+        for (int i : scoville) {
+            pq.offer(i);
+        }
+        
+        while (pq.peek() < K && pq.size() >= 2) {
+            int a = pq.poll();
+            int b = pq.poll();
+            int tmp = a + b * 2;
+            pq.offer(tmp);
+            answer++;
+        }
+        
+        if (pq.peek() < K) answer = -1;
+        return answer;
+    }
+}
