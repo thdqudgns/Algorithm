c349caf0a12a4dcd4491ca1e05c9b2ce6d296863
송병훈 <thdqudgns9612@gmail.com>
2023-04-05 23:42:09 +0900

    [level 1] Title: 신규 아이디 추천, Time: 5.94 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,49 +1,74 @@
 class Solution {
+    public static void main(String[] args) {
+		System.out.println(solution("...!@BaT#*..y.abcdefghijklm"));
+	}
+
     public static String solution(String new_id) {
         String answer = new_id.toLowerCase();
         for (int i = answer.length() - 1; i >= 0; i--) {
 			char tmp = answer.charAt(i);
 			if ('a' <= tmp && tmp <= 'z') continue;
-            if ('0' <= tmp && tmp <= '9') continue;
+			if ('0' <= tmp && tmp <= '9') continue;
 			if (tmp == '-' || tmp == '_' || tmp == '.') continue;
 			answer = answer.substring(0, i) + answer.substring(i+1);
 		}
-        answer = removeDot(answer);
-        answer = checkDot(answer);
-        if (answer.equals("")) answer = "a";
-        answer = checkLenth(answer);
-        answer = addLast(answer);
-        return answer;
+        
+        String result = new ID(answer)
+        		.removeDot()
+        		.checkDot()
+        		.blank()
+        		.checkLenth()
+        		.checkDot()
+        		.addLast()
+        		.getResult();
+        
+        return result;
     }
 
-	private static String removeDot(String answer) {
-		if (answer.length() <= 1) return answer;
-		for (int i = answer.length() - 1; i >= 2; i--) {
-			String dots = ".".repeat(i);
-			if (answer.contains(dots)) answer = answer.replace(dots, ".");
-		}
-		return answer;
-	}
-	
-	private static String checkDot(String answer) {
-		if (answer.length() < 1) return answer;
-		if (answer.lastIndexOf(".") == answer.length() - 1) answer = answer.substring(0, answer.length()-1);
-		if (answer.length() < 1) return answer;
-		if (answer.indexOf(".") == 0) answer = answer.substring(1, answer.length());
-		return answer;
-	}
-
-	private static String checkLenth(String answer) {
-		if (answer.length() < 16) return answer; 
-		answer = answer.substring(0, 15);
-		answer = checkDot(answer);
-		return answer;
-	}
-	
-	private static String addLast(String answer) {
-		if (answer.length() > 2) return answer;
-		if (answer.length() == 2) answer += String.valueOf(answer.charAt(1));
-		else answer += String.valueOf(answer.charAt(0)).repeat(2);
-		return answer;
-	}
+    private static class ID {
+    	private String s;
+    	
+    	ID(String s) {
+    		this.s = s;
+    	}
+    	
+    	private ID removeDot() {
+    		if (s.length() <= 1) return this;
+    		for (int i = s.length() - 1; i >= 2; i--) {
+    			String dots = ".".repeat(i);
+    			if (s.contains(dots)) s = s.replace(dots, ".");
+    		}
+    		return this;
+    	}
+    	
+    	private ID checkDot() {
+    		if (s.length() < 1) return this;
+    		if (s.lastIndexOf(".") == s.length() - 1) s = s.substring(0, s.length()-1);
+    		if (s.length() < 1) return this;
+    		if (s.indexOf(".") == 0) s = s.substring(1, s.length());
+    		return this;
+    	}
+    	
+    	private ID blank() {
+    		s = s.isEmpty() ? "a" : s;
+    		return this;
+    	}
+    	
+    	private ID checkLenth() {
+    		if (s.length() < 16) return this; 
+    		s = s.substring(0, 15);
+    		return this;
+    	}
+    	
+    	private ID addLast() {
+    		if (s.length() > 2) return this;
+    		if (s.length() == 2) s += String.valueOf(s.charAt(1));
+    		else s += String.valueOf(s.charAt(0)).repeat(2);
+    		return this;
+    	}
+    	
+    	private String getResult() {
+    		return s;
+    	}
+    }
 }
