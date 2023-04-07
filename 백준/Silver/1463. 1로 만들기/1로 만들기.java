e9d03bd8985516acbe7eefe0887a941af57aa972
송병훈 <thdqudgns9612@gmail.com>
2023-04-07 22:00:07 +0900

    [Silver III] Title: 1로 만들기, Time: 220 ms, Memory: 63704 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -28,11 +28,11 @@ public class Main {
 	private static int solve(int X) {
 		if (dp[X] != INF) return dp[X];
 		if (X % 6 == 0) {
-			dp[X] = Math.min(dp[X], Math.min(solve(X/3), Math.min(solve(X/2), solve(X-1))) + 1);
+			dp[X] = Math.min(solve(X/3), Math.min(solve(X/2), solve(X-1))) + 1;
 		} else if (X % 3 == 0) {
-			dp[X] = Math.min(dp[X], Math.min(solve(X/3), solve(X-1)) + 1);
+			dp[X] = Math.min(solve(X/3), solve(X-1)) + 1;
 		} else if (X % 2 == 0) {
-			dp[X] = Math.min(dp[X], Math.min(solve(X/2), solve(X-1)) + 1);
+			dp[X] = Math.min(solve(X/2), solve(X-1)) + 1;
 		} else {
 			dp[X] = Math.min(dp[X], solve(X-1) + 1);
 		}
